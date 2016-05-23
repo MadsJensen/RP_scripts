a655c4e876e7a9c79932ffa7ae85ae04ef051cb4
Mads Jensen <mje.mads@gmail.com>
2016-05-23 13:02:25 +0200

    update script

--- xscan_script.py
+++ xscan_script.py
@@ -13,24 +13,21 @@ from stormdb.process import Maxfilter
 
 
 # MAXFILTER PARAMS #
-mf_params = dict(origin='0 0 40',
-                 frame='head',
-                 autobad="off",
-                 st=True,
-                 st_buflen=30,
-                 st_corr=0.95,
-                 movecomp=True,
-                 # cal=cal,
-                 # ctc=ctc,
-                 mx_args='',
-                 maxfilter_bin='maxfilter',
-                 force=True
-                 )
+xscan_params = dict(corr=0.95,
+                    movecomp=True,
+                    # cal=cal,
+                    # ctc=ctc,
+                    mx_args='',
+                    maxfilter_bin='maxfilter',
+                    force=True
+                    )
 
 
 # path to submit_to_isis
 cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
 proj_code = "MINDLAB2011_24-MEG-readiness"
+
+conditions = {"classic": "2", "plan": "4", "interupt": "3"}
 
 db = Query(proj_code)
 proj_folder = os.path.join('/projects', proj_code)
@@ -39,42 +36,35 @@ script_dir = proj_folder + '/scripts/'
 
 included_subjects = db.get_subjects()
 # just test with first one!
-included_subjects = included_subjects[-3:]
+included_subjects = included_subjects[6:]
 
-for sub in included_subjects:
+for j, sub in enumerate(included_subjects):
     # this is an example of getting the DICOM files as a list
-    # sequence_name='t1_mprage_3D_sag'
+#    # sequence_name='t1_mprage_3D_sag'
     MEG_study = db.get_studies(sub, modality='MEG')
-    if len(MEG_study) > 0:
-        # This is a 2D list with [series_name, series_number]
-        series = db.get_series(sub, MEG_study[0], 'MEG')
-        series = series["data"]  # only use the "data" series for now.
-
+#    if len(MEG_study) > 0:
+#        # This is a 2D list with [series_name, series_number]
+#        series = db.get_series(sub, MEG_study[0], 'MEG')
+#                
+#        classic_fname = series.keys()[series.values().index("2")]
+#        interupt_fname = series.keys()[series.values().index("3")]    
+#        plan_fname = series.keys()[series.values().index("4")]                
+#        
+#        classic_serie = series[classic_fname]  # only use the "data" series for now.
+#        interupt_serie = series[interupt_fname]  # only use the "data" series for now.
+#        plan_serie = series[plan_fname]  # only use the "data" series for now.
+#        
     # Change this to be more elegant: check whether any item in series
     # matches sequence_name
-        in_name = db.get_files(sub, MEG_study[0], 'MEG', series)
-        out_name = "%s_%s_mc_raw_tsss.fif" % (sub[:4], "data")
-        # print(out_name)
 
-        # if len(in_name) > 1:
-        for j, in_file in enumerate(in_name):
-            if j == 0:
-                out_fname = maxfiltered_folder + out_name
-            else:
-                out_fname = maxfiltered_folder \
-                            + out_name[:-4] + "-%d.fif" % j
+    for condition in conditions:
+        in_name = db.get_files(sub, MEG_study[0], 'MEG',
+                                   conditions[condition])
+        out_name = "%s_xscan-bads_%s.txt" % (sub[:4], condition)
+            
+        out_fname = maxfiltered_folder + out_name
+        
+        xscan_cmd = "xscan -f %s -corr 0.95 > %s" % (in_name[0], out_fname)        
+        print(xscan_cmd)l
 
-            # print(out_fname)
-            tsss_mc_log = out_fname[:-3] + "log"
-            headpos_log = out_fname[:-4] + "_hp.log"
-
-            # print(tsss_mc_log)
-            # print(headpos_log)
-
-            mf_params["logfile"] = tsss_mc_log
-            mf_params["mv_hp"] = headpos_log
-            mf = Maxfilter(proj_code)
-            mf.build_maxfilter_cmd(in_file, out_fname, **mf_params)
-
-            print(mf.cmd)
-            subprocess.call([cmd, "4", mf.cmd])
+        subprocess.call([cmd, "4", xscan_cmd])
