2f7a9d35b751075afdabb28f2c19e8b7ec9fc800
Mads Jensen <mje.mads@gmail.com>
2016-05-23 14:32:41 +0200

    Clean up

--- xscan_script.py
+++ xscan_script.py
@@ -11,18 +11,6 @@ sys.path.append(path_to_stormdb)
 from stormdb.access import Query
 from stormdb.process import Maxfilter
 
-
-# MAXFILTER PARAMS #
-xscan_params = dict(corr=0.95,
-                    movecomp=True,
-                    # cal=cal,
-                    # ctc=ctc,
-                    mx_args='',
-                    maxfilter_bin='maxfilter',
-                    force=True
-                    )
-
-
 # path to submit_to_isis
 cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
 proj_code = "MINDLAB2011_24-MEG-readiness"
@@ -38,24 +26,10 @@ included_subjects = db.get_subjects()
 # just test with first one!
 included_subjects = included_subjects[6:7]
 
-for j, sub in enumerate(included_subjects):
+for sub in included_subjects:
     # this is an example of getting the DICOM files as a list
 #    # sequence_name='t1_mprage_3D_sag'
     MEG_study = db.get_studies(sub, modality='MEG')
-#    if len(MEG_study) > 0:
-#        # This is a 2D list with [series_name, series_number]
-#        series = db.get_series(sub, MEG_study[0], 'MEG')
-#                
-#        classic_fname = series.keys()[series.values().index("2")]
-#        interupt_fname = series.keys()[series.values().index("3")]    
-#        plan_fname = series.keys()[series.values().index("4")]                
-#        
-#        classic_serie = series[classic_fname]  # only use the "data" series for now.
-#        interupt_serie = series[interupt_fname]  # only use the "data" series for now.
-#        plan_serie = series[plan_fname]  # only use the "data" series for now.
-#        
-    # Change this to be more elegant: check whether any item in series
-    # matches sequence_name
 
     for condition in conditions:
         in_name = db.get_files(sub, MEG_study[0], 'MEG',
