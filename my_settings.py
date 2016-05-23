3f52ff1d417faaf9fd1a4cb152f7d36f24f78a82
Mads Jensen <mje.mads@gmail.com>
2016-05-23 11:38:58 +0200

    fixed typo in data_path

--- my_settings.py
+++ my_settings.py
@@ -3,12 +3,9 @@ import socket
 # Setup paths and prepare raw data
 hostname = socket.gethostname()
 
-if hostname == "wintermute":
-    data_path = "/home/mje/mnt/hyades/scratch4/" +\
-                "MINDLAB2015_MEG-CorticalAlphaAttention/"
-else:
-    data_path = "/projects/MINDLAB2011_24-MEG-readiness/scratch" \
-                "/mne_analysis_new"
+
+data_path = "/projects/MINDLAB2011_24-MEG-readiness/scratch" \
+                "/mne_analysis_new/"
 
 subjects_dir = "/projects/MINDLAB2011_24-MEG-readiness/scratch/mri/"
 save_folder = data_path + "filter_ica_data/"
