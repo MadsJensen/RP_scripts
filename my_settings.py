1baa1b1b5f86b52f552a0225b76449995e18fa3b
Mads Jensen <mje.mads@gmail.com>
2016-05-24 21:36:23 +0200

    Added conditions

--- my_settings.py
+++ my_settings.py
@@ -7,6 +7,8 @@ reject_params = dict(grad=4000e-13,  # T / m (gradiometers)
                      mag=4e-12  # T (magnetometers)
                      )
 
+conditions = ["classic", "plan", "interput"]
+
 data_path = "/projects/MINDLAB2011_24-MEG-readiness/scratch" \
                 "/mne_analysis_new/"
 
