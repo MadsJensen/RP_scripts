3d628015bcf64e40860602d06bc15812fcb0c4e5
Mads Jensen <mje.mads@gmail.com>
2016-05-30 11:31:25 +0200

    Removed sub 0007

--- wrapper_functions.py
+++ wrapper_functions.py
@@ -10,7 +10,7 @@ import subprocess
 
 cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
 
-subjects = ["0007", "0008", "0009", "0010", "0011", "0012", "0013",
+subjects = ["0008", "0009", "0010", "0011", "0012", "0013",
             "0014", "0015","0016", "0017", "0018",  "0019", "0020",
             "0021", "0022"]
 # TODO: fix subjects
