1854653d8880544134a807eb881830ec0ac59ba6
Mads Jensen <mje.mads@gmail.com>
2016-05-30 14:30:54 +0200

    Fixes

--- make_average_plots.py
+++ make_average_plots.py
@@ -6,10 +6,14 @@ Created on Fri May 27 15:53:42 2016
 """
 
 import mne
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
 
 from my_settings import *
 
-subject = "0021"
+subject = sys.argv[1]
 
 epo_cls = mne.read_epochs(epochs_folder + "%s_classic-epo.fif" % subject)
 epo_pln = mne.read_epochs(epochs_folder + "%s_plan-epo.fif" % subject)
@@ -20,6 +24,11 @@ avg_pln = epo_pln.average()
 avg_int = epo_int.average()
 
 
-avg_cls.crop(tmin=-.5, tmax=0).plot_joint(title="classic")
+fig = avg_cls.crop(tmin=-.5, tmax=0).plot_joint(title="classic")
+fig.savefig(epochs_folder + "pics/%s_classic_avg.png" % subject)
+
 avg_pln.crop(tmin=-.5, tmax=0).plot_joint(title="plan")
+fig.savefig(epochs_folder + "pics/%s_plan_avg.png" % subject)
+
 avg_int.crop(tmin=-.5, tmax=0).plot_joint(title="interupt")
+fig.savefig(epochs_folder + "pics/%s_interupt_avg.png" % subject)
