b93714fbe49063c0cd808c20a6638d2abc0e031d
Mads Jensen <mje.mads@gmail.com>
2016-05-31 14:44:12 +0200

    Added script to fix BST bems with meshfix

---
+++ fix_BST_bem.sh
@@ -0,0 +1,16 @@
+
+cd $SUBJECTS_DIR/$1/bem
+
+mris_convert head_bem_12962V.fs head_bem_12962V.stl
+mris_convert innerskull_bem_12962V.fs innerskull_bem_12962V.stl
+mris_convert outerskull_bem_12962V.fs outerskull_bem_12962V.stl
+
+meshfix rh.head_bem_12962V.stl -u 10 --vertices 4098 --fsmesh
+meshfix rh.innerskull_bem_12962V.stl -u 10 --vertices 4098 --fsmesh
+meshfix rh.outerskull_bem_12962V.stl -u 10 --vertices 4098 --fsmesh
+
+mv rh.innerskull_bem_12962V_fixed.fsmesh inner_skull.surf
+mv rh.outerskull_bem_12962V_fixed.fsmesh outer_skull.surf
+mv rh.head_bem_12962V_fixed.fsmesh outer_skin.surf
+
+rm rh*.stl
