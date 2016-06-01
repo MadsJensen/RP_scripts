dca5931e5e29fc666177aa77a5061b6c97e97fa3
Mads Jensen <mje.mads@gmail.com>
2016-06-01 15:04:18 +0200

    Added all the params

--- make_fwd.py
+++ make_fwd.py
@@ -5,6 +5,10 @@ from my_settings import *
 
 subject = sys.argv[1]
 trans = mne_folder + "%s-trans.fif" % subject
+bem = subjects_dir + "%s/bem/%s-8192-8192-8182-bem-sol.fif" % (subject,
+                                                               subject)
+src = mne_folder + "%s-oct-6-src.fif" % subject
+
 
 for condition in conditions:
     raw_fname = maxfiltered_folder + "%s_%s_mc_tsss-raw.fif" % (subject,
