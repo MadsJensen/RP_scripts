a9afae812fdc510bcc8edc5b2f77e425857cbf82
Mads Jensen <mje.mads@gmail.com>
2016-06-01 15:58:27 +0200

    typo fixes

--- calc_noise-cov.py
+++ calc_noise-cov.py
@@ -11,18 +11,20 @@ from my_settings import *
 subject = sys.argv[1]
 
 # SETTINGS
-tmin, tmax = -4, 1
 
 for condition in conditions:
     epochs = mne.read_epochs(epochs_folder + "%s_%s-epo.fif" % (subject,
                                                                 condition))
-    cov = mne.compute_covariance(epochs["press"], tmax=-3.5, method='shrunk')
-    cov.save(mne_folder + "%s_%s-cov.fif" % (subject, condition))
 
     if condition is "interupt":
-        evoked = epochs["press"].average()
+        cov = mne.compute_covariance(epochs["press"], tmax=-3.5,
+                                     method='shrunk')
     else:
-        evoked.average()
+        cov = mne.compute_covariance(epochs, tmax=-3.5, method='shrunk')
 
+    cov.save(mne_folder + "%s_%s-cov.fif" % (subject, condition))
+
+    
+    evoked = epochs["press"].average()
     fig = evoked.plot_white(cov)
     fig.savefig(mne_folder + "plots_cov/%s_%s_cov.png" % (subject, condition))
--- make_inv.py
+++ make_inv.py
@@ -7,12 +7,13 @@ from my_settings import *
 subject = sys.argv[1]
 
 for condition in conditions:
-    fwd = mne.read_forward_solution(mne_folder + "%s_%s-fwd" % (subject,
-                                                                condition),
+    fwd = mne.read_forward_solution(mne_folder + "%s_%s-fwd.fif" % (subject,
+                                                                    condition),
                                     surf_ori=True)
     evoked = mne.read_evokeds(epochs_folder + "%s_%s-ave.fif" % (subject,
                                                                  condition))
-    cov = mne.read(mne_folder + "%s_%s-cov.fif" % (subject, condition))
+    evoked = evoked[0]
+    cov = mne.read_cov(mne_folder + "%s_%s-cov.fif" % (subject, condition))
 
     # make an MEG inverse operator
     inverse_operator = make_inverse_operator(evoked.info, fwd, cov,
