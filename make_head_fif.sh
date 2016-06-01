d5d8aba221aa28101cf43f9799960820ce6230bf
Mads Jensen <mje.mads@gmail.com>
2016-06-01 11:41:32 +0200

    Fixed stupid typo

--- make_head_fif.sh
+++ make_head_fif.sh
@@ -3,5 +3,5 @@ export SUBJECTS_DIR = "/projects/MINDLAB2011_24-MEG-readiness/scratch/fs_subject
 
 for i in $(seq -f "%04g" 8 22)
 do
-    mne_surf2bem --surf $SUBJECTS_DIR/$1/bem/outer_skin.surf --fif $SUBJECTS_DIR/$1/bem/$-head.fif --id 4
+    mne_surf2bem --surf $SUBJECTS_DIR/$i/bem/outer_skin.surf --fif $SUBJECTS_DIR/$i/bem/$i-head.fif --id 4
 done
