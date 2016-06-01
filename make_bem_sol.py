396a0f33866bb3d12d747581ea55d48daf5ab070
Mads Jensen <mje.mads@gmail.com>
2016-06-01 10:55:27 +0200

    Added subjects to file

--- make_bem_sol.py
+++ make_bem_sol.py
@@ -9,8 +9,11 @@ from my_settings import *
 cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
 os.environ["SUBJECTS_DIR"] = subjects_dir
 
-for subject in subjects:
+subjects = ["0008", "0009", "0010", "0011", "0012", "0013",
+            "0014", "0015", "0016", "0017", "0018", "0019", "0020",
+            "0021", "0022"]
 
+for subject in subjects:
     setup_forward = "mne_setup_forward_model --subject %s --surf --ico -6" % (
         subject)
     subprocess.call([cmd, "2", setup_forward])
