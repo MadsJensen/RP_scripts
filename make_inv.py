ec775adab61f2a4a282fe858d4406e5aa6ce2fec
Mads Jensen <mje.mads@gmail.com>
2016-06-01 15:32:43 +0200

    Initial commit

---
+++ make_inv.py
@@ -0,0 +1,22 @@
+import sys
+import mne
+from mne.minimum_norm import make_inverse_operator, write_inverse_operator
+
+from my_settings import *
+
+subject = sys.argv[1]
+
+for condition in conditions:
+    fwd = mne.read_forward_solution(mne_folder + "%s_%s-fwd" % (subject,
+                                                                condition),
+                                    surf_ori=True)
+    evoked = mne.read_evokeds(epochs_folder + "%s_%s-ave.fif" % (subject,
+                                                                 condition))
+    cov = mne.read(mne_folder + "%s_%s-cov.fif" % (subject, condition))
+
+    # make an MEG inverse operator
+    inverse_operator = make_inverse_operator(evoked.info, fwd, cov,
+                                             loose=0.2, depth=0.8)
+
+    write_inverse_operator(mne_folder + "%s_%s-inv.fif" % (subject, condition),
+                           inverse_operator)
