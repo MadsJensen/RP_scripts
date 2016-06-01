d1cfb22d7fb0e56194e62d21ef65c16ed6b1eccf
Mads Jensen <mje.mads@gmail.com>
2016-06-01 15:00:05 +0200

    Initail commit

---
+++ make_fwd.py
@@ -0,0 +1,18 @@
+import mne
+import sys
+
+from my_settings import *
+
+subject = sys.argv[1]
+trans = mne_folder + "%s-trans.fif" % subject
+
+for condition in conditions:
+    raw_fname = maxfiltered_folder + "%s_%s_mc_tsss-raw.fif" % (subject,
+                                                                condition)
+
+    fwd = mne.make_forward_solution(raw_fname, trans=trans, src=src, bem=bem,
+                                    fname=None, meg=True, eeg=False,
+                                    mindist=5.0, n_jobs=1)
+    mne.write_forward_solution(mne_folder + "%s_%s-fwd.fif" % (subject,
+                                                               condition),
+                               fwd, overwrite=True)
