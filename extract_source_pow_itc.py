7c162a90ff9b3c84e2c72f45bab0dc36dd908ed3
Mads Jensen <mje.mads@gmail.com>
2016-06-07 13:13:56 +0200

    Code clean up

--- extract_source_pow_itc.py
+++ extract_source_pow_itc.py
@@ -5,8 +5,7 @@ Created on Wed Jun  1 21:06:28 2016
 @author: mje
 """
 import mne
-from mne.minimum_norm import (read_inverse_operator,
-                              source_induced_power)
+from mne.minimum_norm import (read_inverse_operator, source_induced_power)
 import numpy as np
 import sys
 from my_settings import *
@@ -18,9 +17,9 @@ method = "dSPM"
 freqs = np.arange(6, 90, 3)  # define frequencies of interest
 n_cycles = freqs / 3.  # different number of cycle per frequency
 
-labels = mne.read_labels_from_annot(subject=subject, parc="PALS_B12_Brodmann",
+labels = mne.read_labels_from_annot(subject=subject,
+                                    parc="PALS_B12_Brodmann",
                                     regexp="Brodmann")
-
 
 for condition in conditions:
     inv = read_inverse_operator(mne_folder + "%s_%s-inv.fif" % (subject,
@@ -28,23 +27,24 @@ for condition in conditions:
     epochs = mne.read_epochs(epochs_folder + "%s_%s-epo.fif" % (subject,
                                                                 condition))
     epochs.resample(500, n_jobs=1)
-    power_lbl = np.empty([len(labels,), len(freqs), len(epochs.times)])
-    itc_lbl = np.empty([len(labels,), len(freqs), len(epochs.times)])
-    
-    for j, label in enumerate(labels):  
-        power, itc = source_induced_power(epochs, inv, freqs,
-                                          label, method=method,
+    power_lbl = np.empty([len(labels, ), len(freqs), len(epochs.times)])
+    itc_lbl = np.empty([len(labels, ), len(freqs), len(epochs.times)])
+
+    for j, label in enumerate(labels):
+        power, itc = source_induced_power(epochs,
+                                          inv,
+                                          freqs,
+                                          label,
+                                          method=method,
                                           n_cycles=n_cycles,
                                           baseline=(-3.5, -3.2),
                                           baseline_mode='zscore',
-                                          n_jobs=1, pca=True)
+                                          n_jobs=1,
+                                          pca=True)
         power_lbl[j] = power.mean(axis=0)
         itc_lbl[j] = itc.mean(axis=0)
-       
-       
-    np.save(source_folder + "source_TF/%s_%s_source-pow.npy" % (subject,
-                                                                condition),
-            power)
-    np.save(source_folder + "source_TF/%s_%s_source-itc.npy" % (subject,
-                                                                condition),
-            itc)
+
+    np.save(source_folder + "source_TF/%s_%s_source-pow.npy" %
+            (subject, condition), power)
+    np.save(source_folder + "source_TF/%s_%s_source-itc.npy" %
+            (subject, condition), itc)
