59f597937107df4e4f838b3ded16104ca3c426e0
Mads Jensen <mje.mads@gmail.com>
2016-06-07 20:12:47 +0200

    update for use

--- extract_source_pow_itc.py
+++ extract_source_pow_itc.py
@@ -16,6 +16,8 @@ subject = sys.argv[1]
 method = "dSPM"
 freqs = np.arange(6, 90, 3)  # define frequencies of interest
 n_cycles = freqs / 3.  # different number of cycle per frequency
+snr = 1.
+lambda2 = 1. / snr**2
 
 labels = mne.read_labels_from_annot(subject=subject,
                                     parc="PALS_B12_Brodmann",
@@ -26,17 +28,24 @@ for condition in conditions:
                                                                 condition))
     epochs = mne.read_epochs(epochs_folder + "%s_%s-epo.fif" % (subject,
                                                                 condition))
-    epochs.resample(500, n_jobs=1)
+    # epochs.resample(500, n_jobs=1)
     power_lbl = np.empty([len(labels, ), len(freqs), len(epochs.times)])
     itc_lbl = np.empty([len(labels, ), len(freqs), len(epochs.times)])
 
     for j, label in enumerate(labels):
+        print("\n****************************")
+        print("Working on: %s" % label.name)
+        print("****************************\n")
+        
         power, itc = source_induced_power(epochs,
                                           inv,
                                           freqs,
                                           label,
+                                          lambda2=lambda2,
                                           method=method,
                                           n_cycles=n_cycles,
+                                          decim=2,
+                                          pick_ori="normal",
                                           baseline=(-3.5, -3.2),
                                           baseline_mode='zscore',
                                           n_jobs=1,
