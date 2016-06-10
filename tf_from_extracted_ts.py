c752cbd6293f9d52d6d3eabd7e516f1ff14614a5
Mads Jensen <mje.mads@gmail.com>
2016-06-10 10:21:06 +0200

    Added loop over epochs

--- tf_from_extracted_ts.py
+++ tf_from_extracted_ts.py
@@ -14,8 +14,13 @@ n_cycles = frequencies / 3.
 for condition in conditions:
     ts = np.load(source_folder + "ave_ts/%s_%s_ts-epo.npy" % (subject,
                                                               condition))
-    tfr = cwt_morlet(ts,
-                     sfreq=sfreq,
-                     frequencies=frequencies,
-                     use_fft=True,
-                     n_cycles=n_cycles)
+
+    tfr_all = np.empty(
+        [ts.shape[0], ts.shape[1], frequencies.shape, ts.shape[2]])
+    for j, t in enumerate(ts):
+        tfr = cwt_morlet(ts,
+                         sfreq=sfreq,
+                         frequencies=frequencies,
+                         use_fft=True,
+                         n_cycles=n_cycles)
+        tfr_all[j] = tfr
