a1a7c55932f73616add8224e44d299e57ff53bbc
Mads Jensen <mje.mads@gmail.com>
2016-06-13 21:36:47 +0200

    no resample

--- extract_ts_epochs.py
+++ extract_ts_epochs.py
@@ -20,7 +20,7 @@ for condition in conditions:
                                                                 condition))
     epochs = mne.read_epochs(epochs_folder + "%s_%s-epo.fif" % (subject,
                                                                 condition))
-    epochs.resample(500)
+    # epochs.resample(500)
 
     stcs = apply_inverse_epochs(epochs["press"],
                                 inv,
@@ -30,15 +30,15 @@ for condition in conditions:
     ts = [mne.extract_label_time_course(stc,
                                         labels,
                                         inv["src"],
-                                        mode="mean_flip") for stc in stcs]
+                                        mode="pca_flip") for stc in stcs]
 
-    # for h, tc in enumerate(ts):
-    #     for j, t in enumerate(tc):
-    #         t *= np.sign(t[np.argmax(np.abs(t))])
-    #         tc[j, :] = t
-    #     ts[h] = tc
+    for h, tc in enumerate(ts):
+        for j, t in enumerate(tc):
+            t *= np.sign(t[np.argmax(np.abs(t))])
+            tc[j, :] = t
+        ts[h] = tc
 
     ts = np.asarray(ts)
-    # stc.save(source_folder + "%s_%s_epo" % (subject, condition))
+    stc.save(source_folder + "%s_%s_epo" % (subject, condition))
     np.save(source_folder + "ave_ts/%s_%s_ts-epo.npy" % (subject, condition),
             ts)
