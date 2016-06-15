01ddc99b17d1b05557453085728c4ae6ac72dfad
Mads Jensen <mje.mads@gmail.com>
2016-06-15 14:40:31 +0200

    Code clean up

--- epoching_interupt_subject.py
+++ epoching_interupt_subject.py
@@ -13,26 +13,32 @@ subject = sys.argv[1]
 # SETTINGS
 tmin, tmax = -4, 1
 
-
-raw = mne.io.Raw(save_folder + "%s_interupt_filtered_ica_mc_tsss-raw.fif"
-                 % (subject))
+raw = mne.io.Raw(save_folder + "%s_interupt_filtered_ica_mc_tsss-raw.fif" % (
+    subject))
 events = mne.find_events(raw)
 
 for j in range(len(events)):
-    if events[:, 2][j] == 1 and events[:, 2][j-1] == 2:
+    if events[:, 2][j] == 1 and events[:, 2][j - 1] == 2:
         events[:, 2][j] = 3
 
-
-event_id = {'press': 1,
-            "cue": 2,
-            "cued_press": 3}
+event_id = {'press': 1, "cue": 2, "cued_press": 3}
 
 # Setup for reading the raw data
-picks = mne.pick_types(raw.info, meg=True, eeg=False,
-                       stim=True, eog=True, exclude='bads')
+picks = mne.pick_types(raw.info,
+                       meg=True,
+                       eeg=False,
+                       stim=True,
+                       eog=True,
+                       exclude='bads')
 # Read epochs
-epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
-                    baseline=(None, -3.5), reject=reject_params,
+epochs = mne.Epochs(raw,
+                    events,
+                    event_id,
+                    tmin,
+                    tmax,
+                    picks=picks,
+                    baseline=(None, -3.5),
+                    reject=reject_params,
                     preload=True)
 
 epochs.drop_bad(reject=reject_params)
