3418f2318dd196ccbe85ba7212f50c3e0c3eebf2
Mads Jensen <mje.mads@gmail.com>
2016-06-21 09:26:09 +0200

    typos

--- adaboost_graph_analysis.py
+++ adaboost_graph_analysis.py
@@ -12,8 +12,9 @@ subjects = ["0008", "0009", "0010", "0012", "0013", "0014", "0015", "0016",
 
 for subject in subjects:
     cls = np.load(source_folder + "graph_data/%s_classic_pow_pln.npy" %
-                  subject)
-    pln = np.load(source_folder + "graph_data/%s_plan_pow_pln.npy" % subject)
+                  subject).item()
+    pln = np.load(source_folder + "graph_data/%s_plan_pow_pln.npy" %
+                  subject).item()
 
 results_all = {}
 
