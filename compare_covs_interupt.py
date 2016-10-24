5676b3478b30ee674f33737d3d9fd209a0ed4b12
Mads Jensen <au194693@pc-10-3-149-58.pet.auh.dk>
2016-10-24 10:10:08 +0200

    Split loop over conditions into one condtion per file

---
+++ compare_covs_interupt.py
@@ -0,0 +1,37 @@
+import mne
+import sys
+
+from mne import compute_covariance
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from my_settings import *
+
+reject = dict(
+    grad=4000e-13,  # T / m (gradiometers)
+    mag=4e-12)  # T (magnetometers)
+
+subject = sys.argv[1]
+
+# for condition in conditions:
+
+condition = "interupt"
+
+epochs = mne.read_epochs(epochs_folder + "%s_%s-epo.fif" % (subject, condition))
+epochs.drop_bad(reject)
+
+# Make noise cov
+cov = mne.compute_covariance(
+    epochs,
+    method=['empirical', 'shrunk'],
+    tmin=-0.5,
+    tmax=0.0,
+    return_estimators=True,
+    verbose=True)
+
+evoked = epochs.average()
+fig = evoked.plot_white(cov, show=False)
+fig.suptitle("subject: %s" % subject)
+fig.savefig(mne_folder + "plots_cov/sub_%s_%s.png" % (subject, condition))
