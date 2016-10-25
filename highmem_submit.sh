f73a9278a4a66d1a9c6fc508a64dad9a13be56e3
Mads Jensen <au194693@pc-10-3-149-58.pet.auh.dk>
2016-10-25 10:22:21 +0200

    Added subjects_dir

--- highmem_submit.sh
+++ highmem_submit.sh
@@ -7,6 +7,7 @@ do
     OUTPUT=/projects/MINDLAB2011_24-MEG-readiness/scripts/RP_scripts/highmem_logs/log_${sub}.txt
     SCRIPT=/projects/MINDLAB2011_24-MEG-readiness/scripts/RP_scripts/highmem_scripts/script_${sub}.sh
     SCRIPT_PATH=/projects/MINDLAB2011_24-MEG-readiness/scripts/RP_scripts
+    SUBJECTS_DIR=/projects/MINDLAB2011_24-MEG-readiness/scratch/fs_subjects_dir   
 
 cat <<EOF > ${SCRIPT}
 #!/bin/bash
@@ -17,7 +18,7 @@ PATH=$PATH:/usr/local/mni/bin:/usr/local/cfin/bin:.
 # Setup freesurfer
 
 python $SCRIPT_PATH/$1 ${sub} >>  $OUTPUT 2>&1 
-
+export SUBJECTS_DIR=$SUBJECTS_DIR
 
 EOF
 # Make the new script executable
