c6689201fae177a7c38b8edf61fd5398aaf15e3d
Mads Jensen <au194693@pc-10-3-149-58.pet.auh.dk>
2016-10-26 09:21:10 +0200

    Syntax fix, and refactor

--- graph_pagerank_ada_post.py
+++ graph_pagerank_ada_post.py
@@ -1,19 +1,21 @@
 import numpy as np
 import bct
 from sklearn.externals import joblib
-from my_settings import *
+from my_settings import (bands, source_folder)
 
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.cross_validation import (StratifiedKFold, cross_val_score)
 from sklearn.grid_search import GridSearchCV
 
-subjects = ["0008", "0009", "0010", "0012", "0014", "0015", "0016",
-            "0017", "0018", "0019", "0020", "0021", "0022"]
+subjects = [
+    "0008", "0009", "0010", "0012", "0014", "0015", "0016", "0017", "0018",
+    "0019", "0020", "0021", "0022"
+]
 
 cls_all = []
 pln_all = []
 
-scores_all = np.empty([4, 6)
+scores_all = np.empty([4, 6])
 
 for subject in subjects:
     cls = np.load(source_folder + "graph_data/%s_classic_pow_post.npy" %
@@ -29,13 +31,17 @@ for k, band in enumerate(bands.keys()):
     data_cls = []
     for j in range(len(cls_all)):
         tmp = cls_all[j][band]
-        data_cls.append(np.asarray([bct.centrality.pagerank_centrality(
-            g, d=0.85) for g in tmp]).mean(axis=0))
+        data_cls.append(
+            np.asarray(
+                [bct.centrality.pagerank_centrality(
+                    g, d=0.85) for g in tmp]).mean(axis=0))
     data_pln = []
     for j in range(len(pln_all)):
         tmp = pln_all[j][band]
-        data_pln.append(np.asarray([bct.centrality.pagerank_centrality(
-            g, d=0.85) for g in tmp]).mean(axis=0))
+        data_pln.append(
+            np.asarray(
+                [bct.centrality.pagerank_centrality(
+                    g, d=0.85) for g in tmp]).mean(axis=0))
 
     data_cls = np.asarray(data_cls)
     data_pln = np.asarray(data_pln)
@@ -45,15 +51,18 @@ for k, band in enumerate(bands.keys()):
 
     cv = StratifiedKFold(y, n_folds=6, shuffle=True)
 
-    cv_params = {"learning_rate": np.arange(0.1, 1.1, 0.1),
-                 'n_estimators': np.arange(1, 80, 2)}
+    cv_params = {
+        "learning_rate": np.arange(0.1, 1.1, 0.1),
+        'n_estimators': np.arange(1, 80, 2)
+    }
 
-    grid = GridSearchCV(AdaBoostClassifier(),
-                        cv_params,
-                        scoring='accuracy',
-                        cv=cv,
-                        n_jobs=4,
-                        verbose=1)
+    grid = GridSearchCV(
+        AdaBoostClassifier(),
+        cv_params,
+        scoring='accuracy',
+        cv=cv,
+        n_jobs=4,
+        verbose=1)
     grid.fit(X, y)
     ada_cv = grid.best_estimator_
 
