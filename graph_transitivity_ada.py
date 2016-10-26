5f78a5cbb55ac78516b32ea98fa52d2be27a8537
Mads Jensen <au194693@pc-10-3-149-58.pet.auh.dk>
2016-10-26 13:23:48 +0200

    Added transitivity

--- graph_transitivity_ada.py
+++ graph_transitivity_ada.py
@@ -1,16 +1,21 @@
 import numpy as np
 import bct
-from my_settings import *
+from sklearn.externals import joblib
+from my_settings import (bands, source_folder)
 
-from permute.core import two_sample
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.cross_validation import (StratifiedShuffleSplit, cross_val_score)
+from sklearn.grid_search import GridSearchCV
 
-subjects = ["0008", "0009", "0010", "0012", "0014", "0015", "0016",
-            "0017", "0018", "0019", "0020", "0021", "0022"]
+subjects = [
+    "0008", "0009", "0010", "0012", "0014", "0015", "0016", "0017", "0018",
+    "0019", "0020", "0021", "0022"
+]
 
-
-scores_all = np.empty([4, 10])
 cls_all = []
 pln_all = []
+
+scores_all = np.empty([4, 10])
 
 for subject in subjects:
     cls = np.load(source_folder + "graph_data/%s_classic_pow_pln.npy" %
@@ -22,46 +27,48 @@ for subject in subjects:
     cls_all.append(cls)
     pln_all.append(pln)
 
-
-results_cls = dict()
-results_pln = dict()
-
 for k, band in enumerate(bands.keys()):
     data_cls = []
     for j in range(len(cls_all)):
         tmp = cls_all[j][band]
-        data_cls.append(np.asarray([bct.transitivity_wu(g)
-        for g in tmp]).mean(axis=0))
+        data_cls.append(
+            np.asarray([bct.efficiency_wei(g) for g in tmp]).mean(axis=0))
     data_pln = []
     for j in range(len(pln_all)):
         tmp = pln_all[j][band]
-        data_pln.append(np.asarray([bct.transitivity_wu(g)
-        for g in tmp]).mean(axis=0))
+        data_pln.append(
+            np.asarray([bct.efficiency_wei(g) for g in tmp]).mean(axis=0))
 
     data_cls = np.asarray(data_cls)
     data_pln = np.asarray(data_pln)
-    
-    results_cls[band] = data_cls
-    results_pln[band] = data_pln
 
+    X = np.vstack([data_cls, data_pln])
+    y = np.concatenate([np.zeros(len(data_cls)), np.ones(len(data_pln))])
 
-results_perm = dict()
-for band in bands.keys():
-    print(band)
-    data = {'cls': results_cls[band],
-            'pln':results_pln[band]}
+    cv = StratifiedShuffleSplit(y, test_size=0.1)
 
-    model = best.make_model( data )
+    cv_params = {
+        "learning_rate": np.arange(0.1, 1.1, 0.1),
+        'n_estimators': np.arange(1, 80, 2)
+    }
 
-    M = MCMC(model)
-    M.sample(iter=110000, burn=10000)
-    results_perm[band] = M
+    grid = GridSearchCV(
+        AdaBoostClassifier(),
+        cv_params,
+        scoring='accuracy',
+        cv=cv,
+        n_jobs=1,
+        verbose=1)
+    grid.fit(X, y)
+    ada_cv = grid.best_estimator_
 
+    scores = cross_val_score(ada_cv, X, y, cv=cv)
+    scores_all[k, :] = scores
 
-fig = best.plot.make_figure(M)
-fig.savefig('smart_drug.png',dpi=70)
+    # save the classifier
+    joblib.dump(
+        ada_cv,
+        source_folder + "graph_data/transitivity_ada_pln_%s.plk" % band)
 
-    p, t = two_sample(results_cls[band], results_pln[band], stat='t',
-                      alternative='two-sided')
-    results_perm[band] = p                  
-
+np.save(source_folder + "graph_data/transitivity_scores_all_pln.npy",
+        scores_all)
