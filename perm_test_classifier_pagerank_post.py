0540c75bc2d1285f19408ed68d412bef8463262e
Mads Jensen <au194693@pc-10-3-149-58.pet.auh.dk>
2016-10-26 10:11:58 +0200

    Updated with n_job and refactor

--- perm_test_classifier_pagerank_post.py
+++ perm_test_classifier_pagerank_post.py
@@ -1,15 +1,17 @@
 import numpy as np
 import bct
 from sklearn.externals import joblib
-from my_settings import *
+from my_settings import (bands, source_folder)
 
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.cross_validation import (StratifiedKFold, cross_val_score,
                                       permutation_test_score)
 from sklearn.grid_search import GridSearchCV
 
-subjects = ["0008", "0009", "0010", "0012", "0014", "0015", "0016", "0017",
-            "0018", "0019", "0020", "0021", "0022"]
+subjects = [
+    "0008", "0009", "0010", "0012", "0014", "0015", "0016", "0017", "0018",
+    "0019", "0020", "0021", "0022"
+]
 
 cls_all = []
 pln_all = []
@@ -32,13 +34,17 @@ for k, band in enumerate(bands.keys()):
     data_cls = []
     for j in range(len(cls_all)):
         tmp = cls_all[j][band]
-        data_cls.append(np.asarray([bct.centrality.pagerank_centrality(
-            g, d=0.85) for g in tmp]).mean(axis=0))
+        data_cls.append(
+            np.asarray(
+                [bct.centrality.pagerank_centrality(
+                    g, d=0.85) for g in tmp]).mean(axis=0))
     data_pln = []
     for j in range(len(pln_all)):
         tmp = pln_all[j][band]
-        data_pln.append(np.asarray([bct.centrality.pagerank_centrality(
-            g, d=0.85) for g in tmp]).mean(axis=0))
+        data_pln.append(
+            np.asarray(
+                [bct.centrality.pagerank_centrality(
+                    g, d=0.85) for g in tmp]).mean(axis=0))
 
     data_cls = np.asarray(data_cls)
     data_pln = np.asarray(data_pln)
@@ -51,12 +57,8 @@ for k, band in enumerate(bands.keys()):
     model = joblib.load(source_folder +
                         "graph_data/sk_models/pagerank_ada_pre_%s.plk" % band)
 
-    score, perm_scores, pval = permutation_test_score(model,
-                                                      X,
-                                                      y,
-                                                      cv=cv,
-                                                      n_permutations=5000,
-                                                      n_jobs=4)
+    score, perm_scores, pval = permutation_test_score(
+        model, X, y, cv=cv, n_permutations=5000, n_jobs=1)
 
     result = {"score": score, "perm_scores": perm_scores, "pval": pval}
     results_all[band] = result
--- perm_test_classifier_path-strength_post.py
+++ perm_test_classifier_path-strength_post.py
@@ -1,15 +1,17 @@
 import numpy as np
 import bct
 from sklearn.externals import joblib
-from my_settings import *
+from my_settings import (bands, source_folder)
 
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.cross_validation import (StratifiedKFold, cross_val_score,
                                       permutation_test_score)
 from sklearn.grid_search import GridSearchCV
 
-subjects = ["0008", "0009", "0010", "0012", "0014", "0015", "0016", "0017",
-            "0018", "0019", "0020", "0021", "0022"]
+subjects = [
+    "0008", "0009", "0010", "0012", "0014", "0015", "0016", "0017", "0018",
+    "0019", "0020", "0021", "0022"
+]
 
 cls_all = []
 pln_all = []
@@ -32,13 +34,13 @@ for k, band in enumerate(bands.keys()):
     data_cls = []
     for j in range(len(cls_all)):
         tmp = cls_all[j][band]
-        data_cls.append(np.asarray([bct.strengths_und(g)
-                                    for g in tmp]).mean(axis=0))
+        data_cls.append(
+            np.asarray([bct.strengths_und(g) for g in tmp]).mean(axis=0))
     data_pln = []
     for j in range(len(pln_all)):
         tmp = pln_all[j][band]
-        data_pln.append(np.asarray([bct.strengths_und(g)
-                                    for g in tmp]).mean(axis=0))
+        data_pln.append(
+            np.asarray([bct.strengths_und(g) for g in tmp]).mean(axis=0))
     data_cls = np.asarray(data_cls)
     data_pln = np.asarray(data_pln)
 
@@ -46,17 +48,13 @@ for k, band in enumerate(bands.keys()):
     y = np.concatenate([np.zeros(len(data_cls)), np.ones(len(data_pln))])
 
     cv = StratifiedKFold(y, n_folds=6, shuffle=True)
-    
+
     model = joblib.load(source_folder +
                         "graph_data/sk_models/path-strength_ada_post_%s.plk" %
                         band)
 
-    score, perm_scores, pval = permutation_test_score(model,
-                                                      X,
-                                                      y,
-                                                      cv=cv,
-                                                      n_permutations=5000,
-                                                      n_jobs=4)
+    score, perm_scores, pval = permutation_test_score(
+        model, X, y, cv=cv, n_permutations=5000, n_jobs=1)
 
     result = {"score": score, "perm_scores": perm_scores, "pval": pval}
     results_all[band] = result
