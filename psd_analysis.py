4915ce294ceb51ea5987c6effb337c3aef6460eb
Mads Jensen <mads@cnru.com>
2017-01-13 15:17:17 +0100

    ads

--- psd_analysis.py
+++ psd_analysis.py
@@ -11,13 +11,18 @@ subject = sys.argv[1]
 labels = mne.read_labels_from_annot(
     subject=subject, parc="PALS_B12_Brodmann", regexp="Brodmann")
 
-time_conditions = ["pre", "preact"]
-
-for time_cond in time_conditions:
-    for condition in conditions:
-        
+for condition in conditions:
     stcs = pickle.load(open(source_folder +
                 "%s_%s_psd_pre_epo.pkl" % (subject, condition), 'rb'))
     
-
-
+    psd_data_pre = []
+    for lbl in labels:
+        psd_data.append(np.asarray([stc.in_label(lbl).data for stc in stcs]))
+        
+for condition in conditions:
+    stcs = pickle.load(open(source_folder +
+                "%s_%s_psd_preact_epo.pkl" % (subject, condition), 'rb'))
+    
+    psd_data_preact = []
+    for lbl in labels:
+        psd_data.append(np.asarray([stc.in_label(lbl).data for stc in stcs]))
