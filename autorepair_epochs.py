f928f0bf31eba20b69a6601cbbd602fecc441b56
Mads Jensen <mads@cnru.dk>
2017-01-28 20:56:30 +0100

    test

--- autorepair_epochs.py
+++ autorepair_epochs.py
@@ -101,15 +101,15 @@ for condition in conditions:
     bads_comb = list(set(list(bads_mags) + list(bads_grads)))
     bads_comb.sort()
 
-    idx = np.zeros(len(epochs_clean.get_data()))
-    idx[bads_comb] = 1
-    idx = idx.astype(bool)
+    comb_drop = np.zeros(len(epochs_clean.get_data()))
+    comb_drop[bads_comb] = 1
+    comb_drop = comb_drop.astype(bool)
 
     mag_drops = []
     for i in ar_grad.bad_epochs_idx:
         if i is not ar_mag.bad_epochs_idx:
             mag_drops.append(i)
-                
+
     grad_drops = []
     for i in ar_mag.bad_epochs_idx:
         if i is not ar_grad.bad_epochs_idx:
@@ -117,13 +117,14 @@ for condition in conditions:
 
     epochs_grad_clean.drop(np.asarray(grad_drops))
     epochs_mag_clean.drop(np.asarray(mag_drops))
-    
+
     mag_idx = mne.pick_types(epochs_clean.info, meg="mag")
     grad_idx = mne.pick_types(epochs_clean.info, meg="grad")
 
-    epochs_clean.drop(idx)
+    epochs_clean.drop(comb_drop)
     epochs_clean._data[:, grad_idx, :] = epochs_grad_clean.get_data()
     epochs_clean._data[:, mag_idx, :] = epochs_mag_clean.get_data()
 
     # Save epochs
-    epochs_clean.save(epochs_folder + "%s_%s_clean-epo.fif" % (subject, condition))
+    epochs_clean.save(epochs_folder + "%s_%s_clean-epo.fif" % (subject,
+                                                               condition))
