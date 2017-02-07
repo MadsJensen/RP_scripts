d55f641a94814db27b29de269896799288fcc01f
Mads Jensen <mads@cnru.dk>
2017-02-07 21:54:44 +0100

    update to h5io

--- create_network_matrice.py
+++ create_network_matrice.py
@@ -1,22 +1,23 @@
 import mne
+from mne.externals import h5io
 import sys
 import numpy as np
 from nitime import TimeSeries
 from nitime.analysis import CorrelationAnalyzer
 
-from my_settings import *
+from my_settings import (source_folder, bands)
 
 subject = sys.argv[1]
 
 times = np.arange(-4000, 1001, 1)
 times = times / 1000.
 
-ht_cls = np.load(source_folder + "hilbert_data/%s_classic_ht-epo.npy" %
-                 subject).item()
-ht_pln = np.load(source_folder + "hilbert_data/%s_plan_ht-epo.npy" %
-                 subject).item()
-ht_int = np.load(source_folder + "hilbert_data/%s_interupt_ht-epo.npy" %
-                 subject).item()
+ht_cls = + h5io.read_hdf5("hilbert_data/%s_classic_ht-epo.npy" %
+                          subject)
+ht_pln = h5io.read_hdf5(source_folder + "hilbert_data/%s_plan_ht-epo.npy" %
+                        subject)
+ht_int = h5io.read_hdf5(source_folder + "hilbert_data/%s_interupt_ht-epo.npy" %
+                        subject)
 
 results_cls = {}
 results_pln = {}
@@ -28,37 +29,31 @@ for band in bands.keys():
     corr_int = []
 
     ht_cls_bs = mne.baseline.rescale(
-        np.abs(ht_cls[band])**2,
-        times,
-        baseline=(-3.8, -3.3),
-        mode="zscore")
+        np.abs(ht_cls[band])**2, times, baseline=(-3.8, -3.3), mode="zscore")
 
     ht_pln_bs = mne.baseline.rescale(
-        np.abs(ht_pln[band])**2,
-        times,
-        baseline=(-3.8, -3.3),
-        mode="zscore")
+        np.abs(ht_pln[band])**2, times, baseline=(-3.8, -3.3), mode="zscore")
     ht_int_bs = mne.baseline.rescale(
-        np.abs(ht_pln[band])**2,
-        times,
-        baseline=(-3.8, -3.3),
-        mode="zscore")
+        np.abs(ht_pln[band])**2, times, baseline=(-3.8, -3.3), mode="zscore")
 
     for ts in ht_cls_bs:
-        nits = TimeSeries(ts[:, 1250:1750],
-                          sampling_rate=1000)  # epochs_normal.info["sfreq"])
+        nits = TimeSeries(
+            ts[:, 1250:1750],
+            sampling_rate=1000)  # epochs_normal.info["sfreq"])
 
         corr_cls += [CorrelationAnalyzer(nits)]
 
     for ts in ht_pln_bs:
-        nits = TimeSeries(ts[:, 1250:1750],
-                          sampling_rate=1000)  # epochs_normal.info["sfreq"])
+        nits = TimeSeries(
+            ts[:, 1250:1750],
+            sampling_rate=1000)  # epochs_normal.info["sfreq"])
 
         corr_pln += [CorrelationAnalyzer(nits)]
 
     for ts in ht_int_bs:
-        nits = TimeSeries(ts[:, 1250:1750],
-                          sampling_rate=1000)  # epochs_normal.info["sfreq"])
+        nits = TimeSeries(
+            ts[:, 1250:1750],
+            sampling_rate=1000)  # epochs_normal.info["sfreq"])
 
         corr_int += [CorrelationAnalyzer(nits)]
 
