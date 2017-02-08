2aaf08397b19e0f3f5e93b756a6e01f8030b4ea7
Mads Jensen <mads@cnru.dk>
2017-02-08 10:21:37 +0100

    UPdate to creat for all tois

--- create_network_matrice.py
+++ create_network_matrice.py
@@ -22,47 +22,63 @@ results_cls = {}
 results_pln = {}
 results_int = {}
 
-for band in bands.keys():
-    corr_cls = []
-    corr_pln = []
-    corr_int = []
+tois = {
+    "plan": [1250, 1750],
+    "pre-press": [3500, 4000],
+    "post-press": [4001, 4500]
+}
 
-    ht_cls_bs = mne.baseline.rescale(
-        np.abs(ht_cls[band])**2, times, baseline=(-3.8, -3.3), mode="zscore")
+for toi in tois.keys():
+    for band in bands.keys():
+        corr_cls = []
+        corr_pln = []
+        corr_int = []
 
-    ht_pln_bs = mne.baseline.rescale(
-        np.abs(ht_pln[band])**2, times, baseline=(-3.8, -3.3), mode="zscore")
-    ht_int_bs = mne.baseline.rescale(
-        np.abs(ht_pln[band])**2, times, baseline=(-3.8, -3.3), mode="zscore")
+        ht_cls_bs = mne.baseline.rescale(
+            np.abs(ht_cls[band])**2,
+            times,
+            baseline=(-3.8, -3.3),
+            mode="zscore")
 
-    for ts in ht_cls_bs:
-        nits = TimeSeries(
-            ts[:, 1250:1750],
-            sampling_rate=1000)  # epochs_normal.info["sfreq"])
+        ht_pln_bs = mne.baseline.rescale(
+            np.abs(ht_pln[band])**2,
+            times,
+            baseline=(-3.8, -3.3),
+            mode="zscore")
+        ht_int_bs = mne.baseline.rescale(
+            np.abs(ht_pln[band])**2,
+            times,
+            baseline=(-3.8, -3.3),
+            mode="zscore")
 
-        corr_cls += [CorrelationAnalyzer(nits)]
+        for ts in ht_cls_bs:
+            nits = TimeSeries(
+                ts[:, tois[toi][0]:tois[toi][1]],
+                sampling_rate=1000)  # epochs_normal.info["sfreq"])
 
-    for ts in ht_pln_bs:
-        nits = TimeSeries(
-            ts[:, 1250:1750],
-            sampling_rate=1000)  # epochs_normal.info["sfreq"])
+            corr_cls += [CorrelationAnalyzer(nits)]
 
-        corr_pln += [CorrelationAnalyzer(nits)]
+        for ts in ht_pln_bs:
+            nits = TimeSeries(
+                ts[:, tois[toi][0]:tois[toi][1]],
+                sampling_rate=1000)  # epochs_normal.info["sfreq"])
 
-    for ts in ht_int_bs:
-        nits = TimeSeries(
-            ts[:, 1250:1750],
-            sampling_rate=1000)  # epochs_normal.info["sfreq"])
+            corr_pln += [CorrelationAnalyzer(nits)]
 
-        corr_int += [CorrelationAnalyzer(nits)]
+        for ts in ht_int_bs:
+            nits = TimeSeries(
+                ts[:, tois[toi][0]:tois[toi][1]],
+                sampling_rate=1000)  # epochs_normal.info["sfreq"])
 
-    results_cls[band] = np.asarray([c.corrcoef for c in corr_cls])
-    results_pln[band] = np.asarray([c.corrcoef for c in corr_pln])
-    results_int[band] = np.asarray([c.corrcoef for c in corr_int])
+            corr_int += [CorrelationAnalyzer(nits)]
 
-np.save(source_folder + "graph_data/%s_classic_pow_pln.npy" % subject,
-        results_cls)
-np.save(source_folder + "graph_data/%s_plan_pow_pln.npy" % subject,
-        results_pln)
-np.save(source_folder + "graph_data/%s_interupt_pow_pln.npy" % subject,
-        results_int)
+        results_cls[band] = np.asarray([c.corrcoef for c in corr_cls])
+        results_pln[band] = np.asarray([c.corrcoef for c in corr_pln])
+        results_int[band] = np.asarray([c.corrcoef for c in corr_int])
+
+    np.save(source_folder + "graph_data/%s_classic_pow_%s.npy" %
+            (subject, toi), results_cls)
+    np.save(source_folder + "graph_data/%s_plan_pow_pln.npy" % (subject, toi),
+            results_pln)
+    np.save(source_folder + "graph_data/%s_interupt_pow_pln.npy" %
+            (subject, toi), results_int)
