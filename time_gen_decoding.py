8a1b5420b60e6f7110747d4aa1800bdd3ebcbd74
Mads Jensen <mje.mads@gmail.com>
2017-02-19 16:17:47 +0100

    update to fix epochs

--- time_gen_decoding.py
+++ time_gen_decoding.py
@@ -3,7 +3,7 @@ import numpy as np
 import mne
 from mne.decoding import GeneralizationAcrossTime
 
-from my_settings import (epochs_folder, conditions)
+from my_settings import (epochs_folder)
 
 subject = sys.argv[1]
 
@@ -11,10 +11,17 @@ epochs_classic = mne.read_epochs(epochs_folder + "%s_classic-epo.fif" % (
     subject))
 epochs_plan = mne.read_epochs(epochs_folder + "%s_plan-epo.fif" % (subject))
 
+epochs_plan.event_id["press"] = 2
+epochs_plan.event_id["plan"] = epochs_plan.event_id.pop("press")
+epochs_plan.events[:, 2] = 2
+
+mne.equalize_channels([epochs_classic, epochs_plan])
+mne.epochs.equalize_epoch_counts([epochs_classic, epochs_plan])
+
 epochs = mne.concatenate_epochs([epochs_classic, epochs_plan])
-
+y = np.concatenate((np.zeros(len(epochs["press"])), np.ones(len(epochs["plan"]))))
 gat = GeneralizationAcrossTime(predict_mode='mean-prediction', n_jobs=1)
-gat.fit(epochs[('AudL', 'VisL')], y=viz_vs_auditory_l)
+gat.fit(epochs, y=y)
 
-gat.score(epochs, y=viz_vs_auditory_r)
+gat.score(epochs, y=y)
 gat.plot(title="Temporal Generalization (visual vs auditory): left to right")
