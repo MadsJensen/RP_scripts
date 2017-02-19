c8823963b504e89b9d5ee8b923b0e6df3c894bca
Mads Jensen <mads@cnru.dk>
2017-02-19 16:22:16 +0100

    Added comments and pep fixes

--- time_gen_decoding.py
+++ time_gen_decoding.py
@@ -7,21 +7,29 @@ from my_settings import (epochs_folder)
 
 subject = sys.argv[1]
 
+# Load epochs from both conditions
 epochs_classic = mne.read_epochs(epochs_folder + "%s_classic-epo.fif" % (
     subject))
 epochs_plan = mne.read_epochs(epochs_folder + "%s_plan-epo.fif" % (subject))
 
+# Fix the events for the plan epochs so they can be concatenated
 epochs_plan.event_id["press"] = 2
 epochs_plan.event_id["plan"] = epochs_plan.event_id.pop("press")
 epochs_plan.events[:, 2] = 2
 
+# Equalise channels and epochs, and concatenate epochs
 mne.equalize_channels([epochs_classic, epochs_plan])
 mne.epochs.equalize_epoch_counts([epochs_classic, epochs_plan])
-
 epochs = mne.concatenate_epochs([epochs_classic, epochs_plan])
-y = np.concatenate((np.zeros(len(epochs["press"])), np.ones(len(epochs["plan"]))))
+
+# Setup the y vector and GAT
+y = np.concatenate(
+    (np.zeros(len(epochs["press"])), np.ones(len(epochs["plan"]))))
 gat = GeneralizationAcrossTime(predict_mode='mean-prediction', n_jobs=1)
+
+# Fit model
 gat.fit(epochs, y=y)
 
+# Scoring and visualise result
 gat.score(epochs, y=y)
-gat.plot(title="Temporal Generalization (visual vs auditory): left to right")
+gat.plot(title="Temporal Generalization (Classic vs planning): left to right")
