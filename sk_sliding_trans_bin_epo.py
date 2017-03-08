f9e9e7d3336b9c2932bdc5cb050551eeb170d623
Mads Jensen <mads@cnru.dk>
2017-03-08 13:06:00 +0100

    initial commit

---
+++ sk_sliding_trans_bin_epo.py
@@ -0,0 +1,99 @@
+import numpy as np
+import sys
+from sklearn.externals import joblib
+from mne import create_info, EpochsArray
+from mne.decoding import GeneralizationAcrossTime
+from sklearn.model_selection import (StratifiedKFold, GridSearchCV,
+                                     cross_val_score)
+from sklearn.metrics import roc_auc_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import AdaBoostClassifier
+
+from my_settings import (source_folder, data_path, step_size)
+
+import matplotlib
+matplotlib.use('Agg')
+
+subject = sys.argv[1]
+
+# make time points
+times = np.arange(-4000, 1001, 1)
+times = times / 1000.
+selected_times = times[::step_size]
+
+
+# Custom scorer function to use predict_proba
+def scorer(y_true, y_pred):
+    # Probabilistic estimates are reported for each class. In our case
+    # `y_pred` shape is (n_trials, 2), where `y[:, 0] = 1 - y[:, 1]`.
+    return roc_auc_score(y_true, y_pred[:, 1])
+
+
+cls_all = []
+pln_all = []
+
+cls = np.load(source_folder + "graph_data/%s_cls_pow_sliding.npy" %
+              subject).item()
+
+pln = np.load(source_folder + "graph_data/%s_pln_pow_sliding.npy" %
+              subject).item()
+
+cls_tmp = []
+cls_tmp.append(cls["trans_alpha"])
+cls_tmp.append(cls["trans_beta"])
+cls_tmp.append(cls["trans_gamma_low"])
+cls_tmp.append(cls["trans_gamma_high"])
+
+pln_tmp = []
+pln_tmp.append(pln["trans_alpha"])
+pln_tmp.append(pln["trans_beta"])
+pln_tmp.append(pln["trans_gamma_low"])
+pln_tmp.append(pln["trans_gamma_high"])
+
+cls_all.append(np.asarray(cls_tmp))
+pln_all.append(np.asarray(pln_tmp))
+
+data_cls = np.asarray(cls_all)
+data_pln = np.asarray(pln_all)
+
+data_cls = data_cls.squeeze()
+data_cls = data_cls.swapaxes(2, 0)
+data_cls = data_cls.swapaxes(2, 1)
+
+data_pln = data_pln.squeeze()
+data_pln = data_pln.swapaxes(2, 0)
+data_pln = data_pln.swapaxes(2, 1)
+
+# Setup data for epochs and cross validation
+X = np.vstack([data_cls, data_pln])
+y = np.concatenate([np.zeros(len(data_cls)), np.ones(len(data_pln))])
+cv = StratifiedKFold(n_splits=10, shuffle=True)
+
+# Create epochs to use for classification
+n_trial, n_chan, n_time = X.shape
+events = np.vstack((range(n_trial), np.zeros(n_trial, int), y.astype(int))).T
+chan_names = ['MEG %i' % chan for chan in range(n_chan)]
+chan_types = ['mag'] * n_chan
+sfreq = 250
+info = create_info(chan_names, sfreq, chan_types)
+epochs = EpochsArray(data=X, info=info, events=events, verbose=False)
+epochs.times = selected_times[:n_time]
+
+# fit model and score
+gat = GeneralizationAcrossTime(
+    scorer="accuracy", cv=cv, predict_method="predict")
+gat.fit(epochs, y=y)
+gat.score(epochs, y=y)
+
+# Save model
+joblib.dump(gat, data_path + "decode_time_gen/%s_gat_tr.jl" % subject)
+
+# make matrix plot and save it
+fig = gat.plot(
+    cmap="viridis",
+    title="Temporal Gen (Classic vs planning) for transitivity.")
+fig.savefig(data_path + "decode_time_gen/%s_gat_matrix_tr.png" % subject)
+
+fig = gat.plot_diagonal(
+    chance=0.5, title="Temporal Gen (Classic vs planning) for transitivity")
+fig.savefig(data_path + "decode_time_gen/%s_gat_diagonal_tr.png" % subject)
