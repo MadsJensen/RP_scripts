811f6c0e10d196f5564c39c665ca60e616f1189d
Mads Jensen <mads@cnru.dk>
2017-04-05 09:39:36 +0200

    Changed baseline and PEP8

--- extract_source_pow_itc.py
+++ extract_source_pow_itc.py
@@ -8,20 +8,23 @@ import mne
 from mne.minimum_norm import (read_inverse_operator, source_induced_power)
 import numpy as np
 import sys
-from my_settings import (mne_folder, epochs_folder, source_folder, conditions)
+from my_settings import (mne_folder, epochs_folder, source_folder, conditions,
+                         subjects_dir)
 
 subject = sys.argv[1]
 
 # Settings
 method = "dSPM"
-freqs = np.arange(6, 90, 3)  # define frequencies of interest
+freqs = np.arange(6, 90, 2)  # define frequencies of interest
 n_cycles = freqs / 3.  # different number of cycle per frequency
 snr = 3.
 lambda2 = 1. / snr**2
 
-labels = mne.read_labels_from_annot(subject=subject,
-                                    parc="PALS_B12_Brodmann",
-                                    regexp="Brodmann")
+labels = mne.read_labels_from_annot(
+    subject=subject,
+    parc="PALS_B12_Brodmann",
+    regexp="Brodmann",
+    subjects_dir=subjects_dir)
 
 for condition in conditions:
     inv = read_inverse_operator(mne_folder + "%s_%s-inv.fif" % (subject,
@@ -29,27 +32,30 @@ for condition in conditions:
     epochs = mne.read_epochs(epochs_folder + "%s_%s-epo.fif" % (subject,
                                                                 condition))
     epochs.resample(500, n_jobs=1)
-    power_lbl = np.empty([len(labels, ), len(freqs), len(epochs.times[::2])])
-    itc_lbl = np.empty([len(labels, ), len(freqs), len(epochs.times[::2])])
+    power_lbl = np.empty([len(
+        labels, ), len(freqs), len(epochs.times[::2])])
+    itc_lbl = np.empty([len(
+        labels, ), len(freqs), len(epochs.times[::2])])
 
     for j, label in enumerate(labels):
         print("\n****************************")
         print("Working on: %s" % label.name)
         print("****************************\n")
 
-        power, itc = source_induced_power(epochs,
-                                          inv,
-                                          freqs,
-                                          label,
-                                          lambda2=lambda2,
-                                          method=method,
-                                          n_cycles=n_cycles,
-                                          decim=2,
-                                          pick_ori=None,
-                                          baseline=(-3.5, -3.2),
-                                          baseline_mode='zscore',
-                                          n_jobs=1,
-                                          pca=True)
+        power, itc = source_induced_power(
+            epochs,
+            inv,
+            freqs,
+            label,
+            lambda2=lambda2,
+            method=method,
+            n_cycles=n_cycles,
+            decim=2,
+            pick_ori=None,
+            baseline=(-3.8, -3.4),
+            baseline_mode='zscore',
+            n_jobs=1,
+            pca=True)
         power_lbl[j] = power.mean(axis=0)
         itc_lbl[j] = itc.mean(axis=0)
 
