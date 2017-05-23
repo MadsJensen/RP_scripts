fa26d2c9712e10c73a6da11f1c02376465742975
Mads Jensen <mje.mads@gmail.com>
2017-05-23 14:51:45 +0200

    Convert to mat files

---
+++ save_epochs_mat.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+import scipy
+
+from my_settings import (source_folder, conditions)
+
+subject = sys.argv[1]
+
+for condition in conditions:
+    data = np.load(source_folder + "ave_ts/%s_%s_ts_DKT_snr-3-epo.npy" %
+                   (subject, condition))
+    scipy.io.savemat(source_folder +
+                     "ave_ts/mat_files/%s_%s_ts_DKT_snr-3-epo.npy" %
+                     (subject, condition), dict(data=data))
