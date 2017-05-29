3fcd816255116273d6c94558777d82ae089428f0
Mads Jensen <mje.mads@gmail.com>
2017-05-29 10:48:29 +0200

    Refactor to add subject info

--- graph_char-path_orth_subjects.py
+++ graph_char-path_orth_subjects.py
@@ -12,84 +12,57 @@ subjects = [
 ge_data_all = pd.DataFrame()
 lambda_data_all = pd.DataFrame()
 dia_data_all = pd.DataFrame()
-conditions =['cls', 'cls', 'cls',
-             'cls',
-             'cls',
-             'cls',
-             'cls',
-             'cls',
-             'cls',
-             'cls',
-             'cls',
-             'cls',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln',
-             'pln']
-
+conditions = ['classic', "plan"]
 
 tois = ["pln", "pre-press", "post-press"]
 
 for subject in subjects:
+    print("Working on subject: %s" % subject)
     for toi in tois:
-        cls_all = []
-        pln_all = []
-        for subject in subjects:
-            cls = np.load(source_folder + "graph_data/%s_classic_corr_%s_orth.npy" %
-                          (subject, toi))
+        for condition in conditions:
+            data_all = []
+            pln_all = []
+            # noinspection PyAssignmentToLoopOrWithParameter
+            data = np.load(source_folder +
+                           "graph_data/%s_%s_corr_%s_orth.npy" %
+                           (subject, condition, toi))
 
-            pln = np.load(source_folder + "graph_data/%s_plan_corr_%s_orth.npy" %
-                          (subject, toi))
+            graph_data = [bct.charpath(g) for g in data]
 
-        data_cls = [bct.charpath(g) for g in cls]
-        data_pln = [bct.charpath(g) for g in pln]
+            # calc global efficiency
+            data_ge = np.asarray([g[1] for g in graph_data])
 
-        # calc global efficiency
-        cls_ge = np.asarray([g[1] for g in data_cls])
-        pln_ge = np.asarray([g[1] for g in data_pln])
+            # calc lambda
+            data_lambda = np.asarray([g[0] for g in graph_data])
 
-        # calc lambda
-        cls_lambda = np.asarray([g[0] for g in data_cls])
-        pln_lambda = np.asarray([g[0] for g in data_pln])
+            # calc the diameter of the graph
+            data_dia = np.asarray([g[4] for g in graph_data])
 
-        # calc the diameter of the graph
-        cls_dia = np.asarray([g[4] for g in data_cls])
-        pln_dia = np.asarray([g[4] for g in data_pln])
+            ge_data = pd.DataFrame()
+            lambda_data = pd.DataFrame()
+            dia_data = pd.DataFrame()
 
-        ge_data = pd.DataFrame()
-        lambda_data = pd.DataFrame()
-        dia_data = pd.DataFrame()
+            ge_data["ge"] = ge_data
+            ge_data["measure"] = "ge"
+            ge_data["tio"] = toi
+            ge_data["condition"] = condition
+            ge_data["subject"] = subject
 
-        ge_data["ge"] = np.concatenate((cls_ge, pln_ge))
-        ge_data["measure"] = "ge"
-        ge_data["tio"] = toi
-        ge_data["condition"] = conditions
-        ge_data["subject"] = subject
+            lambda_data["lambda"] = data_lambda
+            lambda_data["measure"] = "lambda"
+            lambda_data["tio"] = toi
+            lambda_data["condition"] = condition
+            lambda_data["subject"] = subject
 
-        lambda_data["lambda"] = np.concatenate((cls_lambda, pln_lambda))
-        lambda_data["measure"] = "lambda"
-        lambda_data["tio"] = toi
-        lambda_data["condition"] = conditions
-        lambda_data["subject"] = subject
+            dia_data["dia"] = data_dia
+            dia_data["measure"] = "dia"
+            dia_data["tio"] = toi
+            dia_data["condition"] = condition
+            dia_data["subject"] = subject
 
-        dia_data["dia"] = np.concatenate((cls_dia, pln_dia))
-        dia_data["measure"] = "dia"
-        dia_data["tio"] = toi
-        dia_data["condition"] = conditions
-        dia_data["subject"] = subject
-
-        ge_data_all = ge_data_all.append(ge_data)
-        lambda_data_all = lambda_data_all.append(lambda_data)
-        dia_data_all = dia_data_all.append(dia_data)
-
+            ge_data_all = ge_data_all.append(ge_data)
+            lambda_data_all = lambda_data_all.append(lambda_data)
+            dia_data_all = dia_data_all.append(dia_data)
 
 ge_data_all.to_csv(results_path + "ge_data_all-tois.csv", index=False)
 lambda_data_all.to_csv(results_path + "lambda_data_all-tois.csv", index=False)
