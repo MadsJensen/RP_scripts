ca71040ac93f1b158f04f187201383e533e20b23
Mads Jensen <mje.mads@gmail.com>
2017-06-01 11:24:15 +0200

    ENH: Added RandomizedLogisticRegression for feature selection
    The RandomizedLogisticRegression for feature selcetion is first used in
    a grid search to find the optimal threshold, then applied to the data.

--- graph_eigen_orth_LR.py
+++ graph_eigen_orth_LR.py
@@ -1,12 +1,14 @@
-import numpy as np
 import bct
+import numpy as np
+import pandas as pd
 from sklearn.externals import joblib
-from my_settings import (source_folder)
-
+from sklearn.linear_model import (LogisticRegression, LogisticRegressionCV,
+                                  RandomizedLogisticRegression)
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import (StratifiedKFold, cross_val_score,
                                      permutation_test_score)
-from sklearn.linear_model import (LogisticRegressionCV, LogisticRegression)
-from sklearn.metrics import roc_auc_score
+
+from my_settings import (source_folder)
 
 subjects = [
     "0008", "0009", "0010", "0012", "0013", "0014", "0015", "0016",
@@ -59,6 +61,7 @@ for toi in tois:
         Cs.append(clf.C_)
         LRs.append(clf)
 
+    # Make LR model from mean of different models
     lr_mean = LogisticRegression()
     lr_mean.coef_ = np.asarray(coefs).mean(axis=0)
     lr_mean.C = np.asarray(Cs).mean()
@@ -67,10 +70,12 @@ for toi in tois:
     lr_coef_mean = np.asarray(coefs).mean(axis=0)
     lr_coef_std = np.asarray(coefs).std(axis=0)
 
+    # Calc roc auc score from mean model
     scores_all[toi] = cross_val_score(
         lr_mean, X, y, scoring="roc_auc", cv=StratifiedKFold(n_splits=6,
                                                              shuffle=True))
 
+    # Test model significiant of mean model w/ permutation test
     score_full_X, perm_scores_full_X, pvalue_full_X = permutation_test_score(
         lr_mean,
         X,
@@ -80,13 +85,73 @@ for toi in tois:
         n_permutations=2000,
         n_jobs=1)
 
+    # Save permutation scores
     scores_perm[toi] = [score_full_X, perm_scores_full_X, pvalue_full_X]
+
+    # RandomizedLogisticRegression feature selection
+    # Grid search for optimal RLR params
+    selection_threshold = np.arange(0.1, 1, 0.05)
+    rlr_grid_search = pd.DataFrame()
+
+    for st in selection_threshold:
+        for i in range(200):
+            print("Working on: %s (%d of 200)" % (st, (i + 1)))
+            rlr = RandomizedLogisticRegression(
+                n_resampling=5000, C=lr_mean.C, selection_threshold=st,
+                n_jobs=1)
+            rlr.fit(X, y)
+            X_rlr = rlr.transform(X)
+
+            if X_rlr.size:
+                cv_scores_rlr = cross_val_score(
+                    lr_mean, X_rlr, y, scoring="roc_auc", cv=StratifiedKFold(
+                        6, shuffle=True))
+
+                rlr_tmp = {
+                    "st": st,
+                    "cv_score": cv_scores_rlr.mean(),
+                    "cv_std": cv_scores_rlr.std(),
+                    "n_features": sum(rlr.get_support())
+                }
+                rlr_grid_search = rlr_grid_search.append(
+                    rlr_tmp, ignore_index=True)
+
+    rlr_grid_search_mean = rlr_grid_search.groupby(by="st").mean()
+    rlr_grid_search_mean["n_feat_std"] = \
+        rlr_grid_search.groupby(by="st").std()["n_features"]
+    rlr_grid_search_mean["cv_score_std"] = rlr_grid_search.groupby(
+        by="st").std()["cv_score"]
+
+    rlr = RandomizedLogisticRegression(
+        n_resampling=5000, C=lr_mean.C, selection_threshold=0.65)
+    rlr.fit(X, y)
+    X_rlr = rlr.transform(X)
+
+    cv_scores_rlr = cross_val_score(
+        lr_mean, X_rlr, y, scoring="roc_auc",
+        cv=StratifiedKFold(6, shuffle=True))
+
+    score_rlr, perm_scores_rlr, pvalue_rlr = permutation_test_score(
+        lr_mean,
+        X_rlr,
+        y,
+        scoring="roc_auc",
+        cv=StratifiedKFold(6, shuffle=True),
+        n_permutations=2000,
+        n_jobs=2)
 
     # save the classifier
     joblib.dump(
         lr_mean,
         source_folder + "graph_data/sk_models/eigen_LR_%s_orth.plk" %
         toi)
+    # save rlr classifier
+    joblib.dump(
+        rlr,
+        source_folder + "graph_data/sk_models/eigen_LR_%s_orth_RLR.plk" %
+        toi)
+    rlr_grid_search_mean.to_csv(
+        "graph_data/sk_models/eigen_LR_%s_orth_RLR_scores.csv")
 
 np.save(source_folder + "graph_data/eigen_scores_all_LR.npy",
         scores_all)
