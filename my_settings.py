040a35d29f2efcfd30c253e38fb8647e23a155c6
Mads Jensen <mje.mads@gmail.com>
2018-01-30 11:02:56 +0100

    Scripts for preporc

---
+++ my_settings.py
@@ -0,0 +1,43 @@
+"""
+These are general settings to be used in the current project.
+
+@author: mje
+@email: mads [] cnru.dk
+"""
+# Base paths
+data_path = "/projects/MINDLAB2011_24-MEG-readiness/scratch"
+misc_folder = "/projects/MINDLAB2011_24-MEG-readiness/misc"
+scripts_folder = "/projects/MINDLAB2011_24-MEG-readiness/scripts"
+
+# Folder paths
+# Beamformer folders
+beamformer_raw = data_path + "bf_raw/"
+beamformer_filters = data_path + "bf_filters/"
+beamformer_source = data_path + "bf_source/"
+beamformer_results = data_path + "bf_results/"
+beamformer_mvpa = data_path + "bf_mvpa/"
+
+# Misc
+reject_params = dict(
+    grad=4000e-13,  # T / m (gradiometers)
+    mag=4e-12,  # T (magnetometers)
+    eeg=180e-6)
+
+bands = ["Alpha", "Beta", "Gamma_low", "Gamma_high_1", "Gamma_high_2"]
+conditions = {"classic": 1, "planning": 2, "interupt": 3}
+
+# Functions
+def get_subjects():
+    subjects = list(
+        pandas.read_csv("subs_long.txt", header=None).iloc[:, 0].get_values())
+    subjects = [s[:4] for s in subjects]
+    subjects.remove("0012")
+    subjects.remove("0013")
+    subjects.remove("0019")
+
+    return subjects
+
+
+# Bands
+iter_freqs = [('Alpha', 8, 12), ('Beta', 13, 30), ('Gamma_low', 30, 45),
+              ('Gamma_high_1', 55, 70), ('Gamma_high_2', 70, 90)]
---
+++ preproc_autoreject.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 14:40:18 2017
+
+@author: mje
+"""
+
+import sys
+import numpy as np
+import mne  # noqa
+from autoreject import (LocalAutoRejectCV, compute_thresholds)
+from functools import partial  # noqa
+from my_settings import beamformer_raw, conditions
+from stormdb.access import Query
+import pickle
+
+subject = sys.argv[1]
+
+proj_name = 'MINDLAB2011_24-MEG-readiness'
+qr = Query(proj_name, verbose=False)
+study = qr.get_studies(subj_id=subject, modality="MEG")[0]
+
+random_state = 3423534231
+
+n_interpolates = np.array([1, 4, 32])
+consensus_percs = np.linspace(0, 1.0, 11)
+n_jobs = 1
+l_freq, h_freq = 1, 95  # High and low frequency setting for the band pass
+n_freq = 50  # notch filter frequency
+decim = 4  # decim value
+
+event_id = dict(
+    bide=11, gide=21, gidet=22, giden=23, mide=31, midet=32, miden=33, nide=41)
+
+tmin, tmax = -0.1, 0.9
+baseline = None  # (-0.1, 0)
+
+iter_freqs = [('Alpha', 8, 12), ('Beta', 13, 30), ('Gamma_low', 30, 45),
+              ('Gamma_high_1', 55, 70), ('Gamma_high_2', 70, 90)]
+
+for condition in conditions.keys():
+    for band, fmin, fmax in iter_freqs:
+        series = qr.get_series(
+            subj_id=subject,
+            study=study,
+            modality="MEG",
+            series=condition[condition])
+
+        raw = mne.io.read_raw_fif(series)
+        raw.info['bads'] = []
+        picks = mne.pick_types(
+            raw.info,
+            meg='grad',
+            eeg=False,
+            stim=False,
+            eog=False,
+            include=[],
+            exclude=[])
+
+        raw.info['projs'] = list()  # remove proj
+
+        raw.resample(500, n_jobs=1)
+        raw.notch_filter(n_freq, fir_design='firwin', n_jobs=n_jobs)
+        raw.filter(fmin, None, fir_design='firwin', n_jobs=n_jobs)
+        raw.filter(None, fmax, fir_design='firwin', n_jobs=n_jobs)
+
+        events = mne.find_events(
+            raw,
+            stim_channel='STI101',
+            shortest_event=2,
+            min_duration=0.004,
+            verbose=True)
+        # adjust time delay from tubes
+        adjustTimeLineBy = -13.6
+        events[:, 0] = [
+            x - np.round(adjustTimeLineBy * 10**-3 * raw.info['sfreq'])
+            for x in events[:, 0]
+        ]
+        # TODO:
+        epochs = mne.Epochs(
+            raw,
+            events,
+            event_id,
+            tmin,
+            tmax,
+            baseline=(None, 0),
+            picks=picks,
+            reject=None,
+            verbose=False,
+            detrend=0,
+            preload=True)
+
+        thresh_func = partial(
+            compute_thresholds,
+            picks=picks,
+            method='random_search',
+            random_state=random_state)
+
+        ar = LocalAutoRejectCV(
+            n_interpolates, consensus_percs, thresh_func=thresh_func)
+        ar.fit(epochs)
+
+        epochs_clean = ar.transform(epochs)
+        epochs_clean.save(beamformer_raw + "%s_%s_%s_ar-epo.fif" %
+                          (subject[:4], condition, band))
+        pickle.dump(ar,
+                    open(beamformer_raw + "%s_%s_%s_ar.pkl" %
+                         (subject[:4], condition, band), "wb"))
