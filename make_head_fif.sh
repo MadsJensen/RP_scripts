eb0c820e88793c9c47e6ed13eb2ae9869a650943
Mads Jensen <mje.mads@gmail.com>
2018-01-30 10:30:40 +0100

    clean up

--- make_head_fif.sh
+++
@@ -1,7 +0,0 @@
-
-export SUBJECTS_DIR = "/projects/MINDLAB2011_24-MEG-readiness/scratch/fs_subjects_dir"
-
-for i in $(seq -f "%04g" 8 22)
-do
-    mne_surf2bem --surf $SUBJECTS_DIR/$i/bem/outer_skin.surf --fif $SUBJECTS_DIR/$i/bem/$i-head.fif --id 4
-done
--- wrapper_perm_tests.sh
+++
@@ -1,5 +0,0 @@
-
-for file in perm_test_classifier*
-do
-    eval submit_to_cluster -q all.q \"python $file\"
-done
