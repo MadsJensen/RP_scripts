69aa429ddfab75147821fd96aaf5c58c13b7b8fe
Mads Jensen <mje.mads@gmail.com>
2018-01-30 19:33:36 +0100

    FIX: typo in load epochs

--- make_src_fwd.py
+++ make_src_fwd.py
@@ -7,13 +7,14 @@ from my_settings import (beamformer_raw, beamformer_source, subjects_dir,
 subject = sys.argv[1]
 
 epochs = mne.read_epochs(
-    beamformer_raw + "%s_equi_Alpha_ar-epo.fif" % subject[:4], preload=False)
+    beamformer_raw + "%s_planning_Alpha_ar-epo.fif" % subject[:4],
+    preload=False)
 
 trans_fname = trans_dir + "%s_planning-trans.fif" % subject
 bem = subjects_dir + "%s/bem/%s-3LBEM-sol.fif" % (subject, subject)
 # t1_fname = op.join(subjects_dir, subject, 'mri/T1.mgz')  # T1 MRI
 
-# set up a volume source space    
+# set up a volume source space
 # src = mne.setup_volume_source_space(subject, pos=5., mri=t1_fname, bem=bem)
 # mne.write_source_spaces(beamformer_source + "%s_vol-src.fif" % subject[:4],
 #                         src)
