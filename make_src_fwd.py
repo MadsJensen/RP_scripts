123e00a4cf9f14af58d0125c0428cf7449d0a7af
Mads Jensen <au194693@pc-10-3-149-203.pet.auh.dk>
2018-02-01 15:16:56 +0100

    ENH: make ffwd for each condition

--- make_src_fwd.py
+++ make_src_fwd.py
@@ -1,13 +1,9 @@
 import mne
 import sys
 from my_settings import (beamformer_raw, beamformer_source, subjects_dir,
-                         trans_dir)
+                         trans_dir, conditions)
 
 subject = sys.argv[1]
-
-epochs = mne.read_epochs(
-    beamformer_raw + "%s_planning_Alpha_ar_grads-epo.fif" % subject[:4],
-    preload=False)
 
 trans_fname = trans_dir + "%s_planning-trans.fif" % subject[:4]
 bem = subjects_dir + "%s/bem/%s-8192-8192-8192-bem-sol.fif" % (subject[:4],
@@ -22,15 +18,24 @@ bem = subjects_dir + "%s/bem/%s-8192-8192-8192-bem-sol.fif" % (subject[:4],
 src = subjects_dir + "%s/bem/%s-oct-6-src.fif" % (subject[:4], subject[:4])
 
 # make leadfield
-fwd = mne.make_forward_solution(
-    epochs.info,
-    trans=trans_fname,
-    src=src,
-    bem=bem,
-    meg=True,
-    eeg=False,
-    n_jobs=1)
 
-# let's save it to disk:
-mne.write_forward_solution(
-    beamformer_source + "%s_cor-fwd.fif" % subject[:4], fwd, overwrite=True)
+for condition in conditions.keys():
+    epochs = mne.read_epochs(
+        beamformer_raw + "%s_%s_Alpha_ar_grads-epo.fif" %
+        (subject[:4], condition),
+        preload=False)
+
+    fwd = mne.make_forward_solution(
+        epochs.info,
+        trans=trans_fname,
+        src=src,
+        bem=bem,
+        meg=True,
+        eeg=False,
+        n_jobs=1)
+
+    # let's save it to disk:
+    mne.write_forward_solution(
+        beamformer_source + "%s_%s_cor-fwd.fif" % (subject[:4], condition),
+        fwd,
+        overwrite=True)
