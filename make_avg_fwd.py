1079fe6cbbbc051886c8da1016e434c34de1b30d
Mads Jensen <mje.mads@gmail.com>
2018-02-02 10:57:29 +0100

    FIX: wrong save name

--- make_avg_fwd.py
+++ make_avg_fwd.py
@@ -1,8 +1,9 @@
 import sys
 
 import mne
+import numpy as np
 
-from my_settings import (beamformer_source)
+from my_settings import (beamformer_source, results_folder)
 
 subject = sys.argv[1]
 
@@ -11,6 +12,10 @@ fwd_pln = mne.read_forward_solution(
 fwd_cls = mne.read_forward_solution(
     beamformer_source + "%s_classic_cor-fwd.fif" % subject[:4])
 
+diff_std = np.std(fwd_cls["sol"]["data"] - fwd_pln["sol"]["data"])
+np.savetxt(results_folder + "%s_fwd_std.csv" % subject[:4], diff_std)
+
 fwd_avg = mne.average_forward_solutions([fwd_pln, fwd_cls])
-mne.write_forward_solution(beamformer_source + "%s_avg_cor-fwd.fif" % subject,
+mne.write_forward_solution(beamformer_source + "%s_avg_cor-fwd.fif" %
+                           subject[:4],
                            fwd_avg)
