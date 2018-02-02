6b4cdd5c2081530214b7d27ca6f620d5280934c0
Mads Jensen <mje.mads@gmail.com>
2018-02-02 12:10:42 +0100

    Script for making and applying BF filters

---
+++ apply_bf_filters.py
@@ -0,0 +1,45 @@
+import mne
+import sys
+import numpy as np
+from scipy.signal import hilbert
+from mne.beamformer import apply_lcmv_epochs
+from my_settings import (beamformer_filters, beamformer_raw,
+                         beamformer_results, bands, conditions)
+
+
+def compute_source_itc(stcs):
+    n_trials = len(stcs)
+
+    tmp = np.zeros(stcs[0].data.shape, dtype=np.complex)
+    for stc in stcs:
+        # divide by amplitude and sum angles
+        tmp += stc.data / abs(stc.data)
+
+    # take absolute value and normalize
+    itc = abs(tmp) / n_trials
+
+    return itc
+
+
+subject = sys.argv[1]
+
+for band in bands:
+    filters = np.load(beamformer_filters + "%s_%s_bf_filters_cor.npy" %
+                      (subject[:4], band)).item()
+
+    for condition in conditions:
+        epochs = mne.read_epochs(beamformer_raw + "%s_%s_%s_ar_grads-epo.fif" %
+                                 (subject[:4], band))
+        epochs.pick_types(meg="grad")
+        epochs_hilb = epochs.copy()
+        epochs_hilb._data = hilbert(epochs.get_data())
+
+        itcs = []  # ITC across freq bands
+        stcs = apply_lcmv_epochs(
+            epochs_hilb, filters=filters, max_ori_out="signed")
+
+        itcs.append(compute_source_itc(stcs))
+        stc_dummy = stcs[0]
+        stc_dummy.data = itcs[0]
+        stc_dummy.save(beamformer_results + "%s_%s_%s_cor" % (subject[:4],
+                                                              condition, band))
---
+++ make_bf_filters.py
@@ -0,0 +1,44 @@
+import sys
+
+import mne
+import numpy as np
+from mne.beamformer import make_lcmv
+from mne.utils import estimate_rank
+from scipy.signal import hilbert
+
+from my_settings import (beamformer_filters, beamformer_raw, beamformer_source,
+                         bands)
+
+subject = sys.argv[1]
+
+fwd = mne.read_forward_solution(
+    beamformer_source + "%s_avg_cor-fwd.fif" % subject[:4])
+
+for band in bands:
+    epochs_cls = mne.read_epochs(
+        beamformer_raw + "%s_classis_%s_ar_grads-epo.fif" % (subject[:4],
+                                                             band))
+    epochs_pln = mne.read_epochs(
+        beamformer_raw + "%s_planning_%s_ar_grads-epo.fif" % (subject[:4],
+                                                              band))
+    epochs_pln.info["dev_head_t"] = epochs_cls.info["dev_head_t"]
+
+    epochs = mne.concatenate_epochs([epochs_cls, epochs_pln])
+    
+    epochs.pick_types(meg="grad")
+    epochs_hilb = epochs.copy()
+    epochs_hilb._data = hilbert(epochs.get_data())
+    data_cov = mne.compute_covariance(epochs, tmin=None, tmax=None)
+
+    rank_cov = estimate_rank(data_cov['data'], tol='auto')
+
+    filters = make_lcmv(
+        epochs.info,
+        fwd,
+        data_cov=data_cov,
+        pick_ori='max-power',
+        weight_norm='nai',
+        reg=0.0)
+
+    np.save(beamformer_filters + "%s_optim_%s_bf_filters_cor.npy" %
+            (subject[:4], band), filters)
