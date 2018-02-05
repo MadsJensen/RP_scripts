b3d791cdb83b87d4e184ef8bb5545c4f07538435
Mads Jensen <mje.mads@gmail.com>
2018-02-05 12:10:30 +0100

    Scripts for permutation tests

---
+++ submit_perm_scripts.sh
@@ -0,0 +1,4 @@
+submit_to_cluster "python perm_test_source_st_FS.py Beta" -q highmem.q -n 1
+submit_to_cluster "python perm_test_source_st_FS.py Gamma_low" -q highmem.q -n 1
+submit_to_cluster "python perm_test_source_st_FS.py Gamma_high_1" -q highmem.q -n 1
+submit_to_cluster "python perm_test_source_st_FS.py Gamma_high_2" -q highmem.q -n 1
