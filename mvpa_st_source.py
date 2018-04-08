b596d3237ada80295f5976bd574dcf652a6bb3ac
Mads Jensen <mje.mads@gmail.com>
2018-04-08 08:38:57 +0200

    ENH: better feature selection

--- mvpa_st_source.py
+++ mvpa_st_source.py
@@ -11,6 +11,8 @@ from mne.decoding import (SlidingEstimator, cross_val_multiscore, LinearModel)
 
 from my_settings import beamformer_mvpa, bands
 
+n_jobs = 4
+
 for band in bands:
     X = np.load(beamformer_mvpa + "X_cls_v_pln_%s.npy" % (band))
     y = np.load(beamformer_mvpa + "y_cls_v_pln.npy")
@@ -19,16 +21,22 @@ for band in bands:
 
     clf = make_pipeline(
         StandardScaler(),  # z-score normalization
-        SelectPercentile(f_classif, percentile=40),
-        SelectFromModel(LassoCV(normalize=False), threshold="2*mean"),
+        # SelectPercentile(f_classif, percentile=40),
+        SelectFromModel(
+            LassoCV(
+                cv=StratifiedKFold(n_splits=5, shuffle=True),
+                max_iter=5000,
+                n_jobs=n_jobs),
+            threshold="0.1*mean"),
         LinearModel(LogisticRegression(C=1)))
 
     time_decod = SlidingEstimator(clf, n_jobs=2, scoring='roc_auc')
 
     time_decod.fit(X, y)
-    joblib.dump(time_decod,
-                beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_sfm.jbl" %
-                (band))
+    joblib.dump(
+        time_decod,
+        beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_sfm.jbl" %
+        (band))
 
     scores = cross_val_multiscore(time_decod, X, y, cv=cv)
     np.save(beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_sfm.npy" %
