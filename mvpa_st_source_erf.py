fd5ae76e7fae1e0001e8b39ed42c8f3d7bb6091e
Mads Jensen <mje.mads@gmail.com>
2019-02-18 21:06:53 +0100

    ENH: try selectfrommodel

--- mvpa_st_source_erf.py
+++ mvpa_st_source_erf.py
@@ -3,10 +3,11 @@ import h5io
 import numpy as np
 from sklearn.externals import joblib
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import SelectFromModel
 
 from mne.decoding import (SlidingEstimator, cross_val_multiscore, LinearModel)
 
@@ -25,14 +26,12 @@ cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 
 clf = make_pipeline(
     StandardScaler(),  # z-score normalization
-    SelectKBest(k=250),
+    SelectFromModel(LinearSVC(penalty="l1")),
     LinearModel(LogisticRegression(C=1)))
 time_decod = SlidingEstimator(clf, n_jobs=n_jobs, scoring='roc_auc')
 
 time_decod.fit(X, y)
-joblib.dump(time_decod,
-            erf_mvpa + "source_cls_v_pln_evk_logreg_erf_kbst.jbl")
+joblib.dump(time_decod, erf_mvpa + "source_cls_v_pln_evk_logreg_erf_sfm.jbl")
 
 scores = cross_val_multiscore(time_decod, X, y, cv=cv)
-h5io.write_hdf5(erf_mvpa + "source_cls_v_pln_evk_logreg_erf_kbst.hd5",
-                scores)
+h5io.write_hdf5(erf_mvpa + "source_cls_v_pln_evk_logreg_erf_sfm.hd5", scores)
