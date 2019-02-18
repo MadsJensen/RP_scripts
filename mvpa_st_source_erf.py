9ff05dae78c09580ecc57f23bff965777837ca06
Mads Jensen <mje.mads@gmail.com>
2019-02-18 20:41:36 +0100

    ENH: added feature selection

--- mvpa_st_source_erf.py
+++ mvpa_st_source_erf.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.feature_selection import SelectFpr, f_classif
 
 from mne.decoding import (SlidingEstimator, cross_val_multiscore, LinearModel)
 
@@ -25,13 +25,14 @@ cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 
 clf = make_pipeline(
     StandardScaler(),  # z-score normalization
+    SelectFpr(),
     LinearModel(LogisticRegression(C=1)))
 time_decod = SlidingEstimator(clf, n_jobs=n_jobs, scoring='roc_auc')
 
 time_decod.fit(X, y)
 joblib.dump(time_decod,
-            erf_mvpa + "source_cls_v_pln_evk_logreg_erf_full.jbl")
+            erf_mvpa + "source_cls_v_pln_evk_logreg_erf_fpr.jbl")
 
 scores = cross_val_multiscore(time_decod, X, y, cv=cv)
-h5io.write_hdf5(erf_mvpa + "source_cls_v_pln_evk_logreg_erf_full.npy",
+h5io.write_hdf5(erf_mvpa + "source_cls_v_pln_evk_logreg_erf_fpr.hd5",
                 scores)
