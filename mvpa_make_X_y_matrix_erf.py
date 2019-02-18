d9877dda9305f5084611fba610c42c9cc61300f2
Mads Jensen <mje.mads@gmail.com>
2019-02-18 16:09:03 +0100

    ENH: added rolling mean

--- mvpa_make_X_y_matrix_erf.py
+++ mvpa_make_X_y_matrix_erf.py
@@ -1,17 +1,21 @@
 import numpy as np
 import mne
-from my_settings import (erf_raw, erf_mvpa, subjects, conditions)
+from my_settings import (erf_raw, erf_mvpa, subjects, conditions,
+                         make_rolling_mean)
 
 tmp = mne.read_epochs(erf_raw + "0016_classic_ar_grads_erf-epo.fif")
 data_shape = tmp.get_data().shape
 
+windows_size = 5
+
 for condition in conditions:
-    X = np.zeros((len(subjects), data_shape[1], data_shape[2]))
+    X = np.zeros((len(subjects), data_shape[1],
+                  data_shape[2] - (windows_size * 2)))
 
     for jj, subject in enumerate(subjects):
         epo = mne.read_epochs(erf_raw + "%s_%s_ar_grads_erf-epo.fif" %
                               (subject[:4], condition))
 
-        X[jj] = epo.average().data
+        X[jj] = make_rolling_mean(epo.average().data)
 
-    np.save(erf_mvpa + "X_%s_erf.npy" % condition, X)
+    np.save(erf_mvpa + "X_%s_erf_RM.npy" % condition, X)
--- my_settings.py
+++ my_settings.py
@@ -14,7 +14,6 @@ subjects_dir = data_path + "fs_subjects_dir/"
 trans_dir = data_path + "trans/"
 results_folder = "/projects/MINDLAB2011_24-MEG-readiness/result/"
 
-
 # Beamformer folders
 beamformer_raw = data_path + "bf_raw/"
 beamformer_filters = data_path + "bf_filters/"
@@ -29,7 +28,6 @@ erf_source = data_path + "erf_source/"
 erf_results = data_path + "erf_results/"
 erf_mvpa = data_path + "erf_mvpa/"
 
-
 # Misc
 reject_params = dict(
     grad=4000e-13,  # T / m (gradiometers)
@@ -39,22 +37,17 @@ reject_params = dict(
 bands = ["Alpha", "Beta", "Gamma_low", "Gamma_high_1", "Gamma_high_2"]
 conditions = {"classic": 2, "planning": 4, "interupt": 3}
 
-
-# Functions
-def get_subjects():
-    subjects = list(
-        pandas.read_csv("subs_long.txt", header=None).iloc[:, 0].get_values())
-    subjects = [s[:4] for s in subjects]
-    subjects.remove("0012")
-    subjects.remove("0013")
-    subjects.remove("0019")
-
-    return subjects
-
-
-subjects = ["0008", "0009", "0010", "0012", "0013", "0014", "0015", "0016",
-            "0017", "0018", "0019", "0020", "0021", "0022"]
+subjects = [
+    "0008", "0009", "0010", "0012", "0013", "0014", "0015", "0016", "0017",
+    "0018", "0019", "0020", "0021", "0022"
+]
 
 # Bands
 iter_freqs = [('Alpha', 8, 12), ('Beta', 13, 30), ('Gamma_low', 30, 45),
               ('Gamma_high_1', 55, 70), ('Gamma_high_2', 70, 90)]
+
+
+# Functions
+def make_rolling_mean(evk, windows_size=20):
+    df = evk.to_data_frame()
+    return df.rolling(window=windows_size, center=True).mean().values.T
