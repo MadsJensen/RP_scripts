52a5a981ced63bef1141605a8cd3392b41e01c11
Mads Jensen <mje.mads@gmail.com>
2019-02-19 16:11:18 +0100

    ENH: clean up

--- hyper_params_search.py
+++ hyper_params_search.py
@@ -1,16 +1,15 @@
 import sys
+
 import h5io
 import pandas as pd
-
 from sklearn.externals import joblib
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_selection import SelectKBest, mutual_info_classif
-
 from skopt import BayesSearchCV
-from skopt.space import Real, Integer
+from skopt.space import Integer
 
 from my_settings import erf_mvpa
 
@@ -27,18 +26,14 @@ X = Xy['X'][:, :, windows_size:-windows_size]
 y = Xy['y']
 
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-cv_lss = StratifiedKFold(n_splits=4, shuffle=True, random_state=seed_cv)
 
 pipe = make_pipeline(
     StandardScaler(),  # z-score normalization
     SelectKBest(k=100, score_func=mutual_info_classif),
     LogisticRegression(C=1, tol=tol, solver='lbfgs'))
 
-# pipe = Pipeline([('std_X', StandardScaler()), ('kbest', SelectKBest()),
-#                          ('model', LogisticRegression(C=1, tol=0.0001))])
 search_space = {
-    'logisticregression__C': Real(1e-4, 1e+3, 'log-uniform'),
-    'selectkbest__k': Integer(100, 2000),
+    'selectkbest__k': Integer(1200, 2000),
 }
 
 opt = BayesSearchCV(
@@ -47,13 +42,14 @@ opt = BayesSearchCV(
     cv=cv,
     n_jobs=n_jobs,
     verbose=1,
-    n_iter=200,
-    scoring='roc_auc')
+    n_iter=50,
+    scoring='roc_auc',
+    random_state=seed_cv)
 
 results = pd.DataFrame()
 opts = []
 for ii in (range(Xy['X'].shape[-1])):
-    if ii > 44:  # Skip baseline
+    if ii > 200:  # Skip baseline
         print('working on time %s' % (ii))
         X = Xy["X"][:, :, ii]
 
@@ -71,4 +67,4 @@ for ii in (range(Xy['X'].shape[-1])):
 
         results.to_csv(erf_mvpa + 'skopt_results_lr_kbest_C.csv', index=False)
 
-    joblib.dump(opts, erf_mvpa + 'skopt_results_lr_opts.pkl')
+joblib.dump(opts, erf_mvpa + 'skopt_results_lr_opts.pkl')
