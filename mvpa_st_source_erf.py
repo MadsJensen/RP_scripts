e8f3397ba3afefdbde64179ab3859f9eac301850
Mads Jensen <mje.mads@gmail.com>
2019-02-19 07:42:33 +0100

    ENH: try lasso

--- mvpa_st_source_erf.py
+++ mvpa_st_source_erf.py
@@ -2,8 +2,7 @@ import sys
 import h5io
 import numpy as np
 from sklearn.externals import joblib
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression, LassoCV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
@@ -17,24 +16,26 @@ windows_size = 10
 n_jobs = int(sys.argv[1])
 
 seed = 352341561
+seed_cv = 2423423
 
 Xy = h5io.read_hdf5(erf_mvpa + "Xy_cls_v_pln_erf_RM.hd5")
 X = Xy['X'][:, :, windows_size:-windows_size]
 y = Xy['y']
 
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+cv_lss = StratifiedKFold(n_splits=4, shuffle=True, random_state=seed_cv)
 
 clf = make_pipeline(
     StandardScaler(),  # z-score normalization
-    SelectFromModel(LinearSVC(penalty="l1", dual=False)),
+    SelectFromModel(LassoCV(cv=cv_lss), threshold=0.25),
     LinearModel(LogisticRegression(C=1)))
 time_decod = SlidingEstimator(clf, n_jobs=n_jobs, scoring='roc_auc')
 
 time_decod.fit(X, y)
-joblib.dump(time_decod, erf_mvpa + "source_cls_v_pln_evk_logreg_erf_sfm.jbl")
+joblib.dump(time_decod, erf_mvpa + "source_cls_v_pln_evk_logreg_erf_lss.jbl")
 
 scores = cross_val_multiscore(time_decod, X, y, cv=cv)
 h5io.write_hdf5(
-    erf_mvpa + "source_cls_v_pln_evk_logreg_erf_sfm.hd5",
+    erf_mvpa + "source_cls_v_pln_evk_logreg_erf_lss.hd5",
     scores,
     overwrite=True)
