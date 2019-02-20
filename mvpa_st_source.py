635957c37417a020270e60e14735d09db798eea3
Mads Jensen <mje.mads@gmail.com>
2019-02-20 22:47:58 +0100

    ENH: use RM data

--- mvpa_st_source.py
+++ mvpa_st_source.py
@@ -1,43 +1,38 @@
-import numpy as np
+import sys
+
+import h5io
+from mne.decoding import (SlidingEstimator, cross_val_multiscore, LinearModel)
 from sklearn.externals import joblib
-from sklearn.linear_model import LogisticRegression, LassoCV
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_selection import (SelectFromModel, SelectPercentile,
-                                       f_classif)
-
-from mne.decoding import (SlidingEstimator, cross_val_multiscore, LinearModel)
 
 from my_settings import beamformer_mvpa, bands
 
-n_jobs = 4
+n_jobs = int(sys.argv[1])
+seed = 234351
+tol = 1e-5
 
 for band in bands:
-    X = np.load(beamformer_mvpa + "X_cls_v_pln_%s.npy" % (band))
-    y = np.load(beamformer_mvpa + "y_cls_v_pln.npy")
+    Xy = h5io.read_hdf5(beamformer_mvpa + "Xy_cls_v_pln_%s_RM.npy" % band)
+    X = Xy['X']
+    y = Xy['y']
 
-    cv = StratifiedKFold(n_splits=5, shuffle=True)
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 
     clf = make_pipeline(
         StandardScaler(),  # z-score normalization
-        # SelectPercentile(f_classif, percentile=40),
-        SelectFromModel(
-            LassoCV(
-                cv=StratifiedKFold(n_splits=5, shuffle=True),
-                max_iter=5000,
-                n_jobs=n_jobs),
-            threshold="0.1*mean"),
-        LinearModel(LogisticRegression(C=1)))
+        LinearModel(LogisticRegression(C=1, tol=tol, solver='lbgfs')))
 
-    time_decod = SlidingEstimator(clf, n_jobs=2, scoring='roc_auc')
+    time_decod = SlidingEstimator(clf, n_jobs=n_jobs, scoring='roc_auc')
 
     time_decod.fit(X, y)
     joblib.dump(
         time_decod,
-        beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_sfm.jbl" %
-        (band))
+        beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_RM.jbl" % band)
 
     scores = cross_val_multiscore(time_decod, X, y, cv=cv)
-    np.save(beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_sfm.npy" %
-            (band), scores)
+    h5io.write_hdf5(
+        beamformer_mvpa + "source_cls_v_pln_itc_evk_logreg_%s_RM.hd5" % band,
+        scores)
