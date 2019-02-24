c5d65b0c21908b4b8b6c38058329d66c048fb2b7
Mads Jensen <mje.mads@gmail.com>
2019-02-24 10:07:42 +0100

    ENH: cv over time

--- mvpa_search_events.py
+++ mvpa_search_events.py
@@ -36,33 +36,41 @@ for band in bands:
     stdscl = StandardScaler()
     clf = LogisticRegression(tol=tol, C=1, solver='lbfgs')
     scores = []
-    for time in range(time_idx_start, time_idx_end, 1):
-        for train, test in cv.split(X, y):
+    for train, test in cv.split(X, y):
+        tmp_models = []
+        tmp_std = []
+        for time in range(time_idx_start, time_idx_end, 1):
             X_train, X_test = X[train, :, time], X[test, :, time]
             y_train, y_test = y[train], y[test]
             stdscl.fit(X_train)
             X_train = stdscl.transform(X_train)
             X_test = stdscl.transform(X_test)
             clf.fit(X_train, y_train)
-            scores.append(roc_auc_score(y_test, clf.predict(X_test)))
-            models.append(clf)
-            std.append(stdscl)
+            tmp_models.append(clf)
+            tmp_std.append(stdscl)
 
-    mean_coefs = np.mean([c.coef_ for c in models], axis=0)
-    mean_model = models[0]
-    mean_model.coef_ = mean_coefs
+        mean_coefs = np.mean([c.coef_ for c in tmp_models], axis=0)
+        mean_model = tmp_models[0]
+        mean_model.coef_ = mean_coefs
 
-    mean_std = std[0]
-    mean_std.mean_ = np.mean([c.mean_ for c in std], axis=0)
-    mean_std.var_ = np.mean([c.var_ for c in std], axis=0)
-    mean_std.scale_ = np.mean([c.scale_ for c in std], axis=0)
+        mean_std = tmp_std[0]
+        mean_std.mean_ = np.mean([c.mean_ for c in tmp_std], axis=0)
+        mean_std.var_ = np.mean([c.var_ for c in tmp_std], axis=0)
+        mean_std.scale_ = np.mean([c.scale_ for c in tmp_std], axis=0)
 
-    rocs = np.zeros(X.shape[-1])
+        models.append(mean_model)
+        std.append(mean_std)
+        rocs = np.zeros(X.shape[-1])
 
-    for tt in range(X.shape[-1]):
-        X_std = mean_std.transform(X[:, :, tt])
-        rocs[tt] = roc_auc_score(y, mean_model.predict(X_std))
-        h5io.write_hdf5(
-            beamformer_mvpa + 'mean_model_rocs_%s_roc.hd5' % band,
-            rocs,
-            overwrite=True)
+        for tt in range(X.shape[-1]):
+            X_std = mean_std.transform(X[test, :, tt])
+            rocs[tt] = roc_auc_score(y[test], mean_model.predict(X_std))
+
+        scores.append(rocs)
+
+    scores = np.asarray(scores)
+
+    h5io.write_hdf5(
+        beamformer_mvpa + 'mean_model_rocs_%s_roc.hd5' % band,
+        scores,
+        overwrite=True)
