02aa5cbb86b9d2c6d41e0e23de37ecb869678846
Mads Jensen <mje.mads@gmail.com>
2019-02-24 21:19:01 +0100

    ENH: cont for better indexing

--- mvpa_search_events.py
+++ mvpa_search_events.py
@@ -30,46 +30,50 @@ for jj, band in enumerate(bands):
     y = Xy['y']
 
     # TODO Better time index
-    if (times[clusters[jj][0][0]] < -2) and (times[clusters[jj][0][1]] > -2.5):
-        time_idx_start = clusters[jj][0][0]
-        time_idx_end = clusters[jj][1][1]
+    for cluster in clusters[jj]:
+        if ((times[cluster[0]] > -2.) and (times[cluster[1]] <
+                                                  -2.5)) and\
+                (times[cluster[0]] < -2.5):
+            print('sfd')
+            time_idx_start = clusters[jj][0][0]
+            time_idx_end = clusters[jj][1][1]
 
-        models = []
-        std = []
-        stdscl = StandardScaler()
-        clf = LogisticRegression(tol=tol, C=1, solver='lbfgs')
-        scores = []
-        for train, test in cv.split(X, y):
-            tmp_models = []
-            tmp_std = []
-            for time in range(time_idx_start, time_idx_end, 1):
-                X_train, X_test = X[train, :, time], X[test, :, time]
-                y_train, y_test = y[train], y[test]
-                stdscl.fit(X_train)
-                X_train = stdscl.transform(X_train)
-                X_test = stdscl.transform(X_test)
-                clf.fit(X_train, y_train)
-                tmp_models.append(clf)
-                tmp_std.append(stdscl)
+            models = []
+            std = []
+            stdscl = StandardScaler()
+            clf = LogisticRegression(tol=tol, C=1, solver='lbfgs')
+            scores = []
+            for train, test in cv.split(X, y):
+                tmp_models = []
+                tmp_std = []
+                for time in range(time_idx_start, time_idx_end, 1):
+                    X_train, X_test = X[train, :, time], X[test, :, time]
+                    y_train, y_test = y[train], y[test]
+                    stdscl.fit(X_train)
+                    X_train = stdscl.transform(X_train)
+                    X_test = stdscl.transform(X_test)
+                    clf.fit(X_train, y_train)
+                    tmp_models.append(clf)
+                    tmp_std.append(stdscl)
 
-            mean_coefs = np.mean([c.coef_ for c in tmp_models], axis=0)
-            mean_model = tmp_models[0]
-            mean_model.coef_ = mean_coefs
+                mean_coefs = np.mean([c.coef_ for c in tmp_models], axis=0)
+                mean_model = tmp_models[0]
+                mean_model.coef_ = mean_coefs
 
-            mean_std = tmp_std[0]
-            mean_std.mean_ = np.mean([c.mean_ for c in tmp_std], axis=0)
-            mean_std.var_ = np.mean([c.var_ for c in tmp_std], axis=0)
-            mean_std.scale_ = np.mean([c.scale_ for c in tmp_std], axis=0)
+                mean_std = tmp_std[0]
+                mean_std.mean_ = np.mean([c.mean_ for c in tmp_std], axis=0)
+                mean_std.var_ = np.mean([c.var_ for c in tmp_std], axis=0)
+                mean_std.scale_ = np.mean([c.scale_ for c in tmp_std], axis=0)
 
-            models.append(mean_model)
-            std.append(mean_std)
-            rocs = np.zeros(X.shape[-1])
+                models.append(mean_model)
+                std.append(mean_std)
+                rocs = np.zeros(X.shape[-1])
 
-            for tt in range(X.shape[-1]):
-                X_std = mean_std.transform(X[test, :, tt])
-                rocs[tt] = roc_auc_score(y[test], mean_model.predict(X_std))
+                for tt in range(X.shape[-1]):
+                    X_std = mean_std.transform(X[test, :, tt])
+                    rocs[tt] = roc_auc_score(y[test], mean_model.predict(X_std))
 
-            scores.append(rocs)
+                scores.append(rocs)
 
     scores = np.asarray(scores)
 
