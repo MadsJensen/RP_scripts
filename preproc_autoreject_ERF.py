2863a3064b55b45b1c4df2cc429ec71f409fcac5
Mads Jensen <mje.mads@gmail.com>
2020-06-18 09:07:06 +0200

    ENH: updated parameters

--- preproc_autoreject_ERF.py
+++ preproc_autoreject_ERF.py
@@ -23,79 +23,75 @@ study = qr.get_studies(subj_id=subject, modality="MEG")[0]
 
 random_state = 3423534231
 
-n_interpolates = np.array([1, 4, 8, 12, 16, 32])
-consensus_percs = np.linspace(0, 1.0, 11)
+n_interpolates = np.arange(1, 25, 1)
+consensus = np.linspace(0, 1.0, 11)
+l_freq, h_freq = 1, 90  # High and low frequency setting for the band pass
+
 n_jobs = 1
 l_freq, h_freq = 1, 95  # High and low frequency setting for the band pass
 n_freq = 50  # notch filter frequency
 decim = 4  # decim value
 
 event_id = {"press": 1}
-
 tmin, tmax = -3.5, 0.5
-baseline = None  # (-0.1, 0)
-
-fmin, fmax = 1, 40
+baseline = (None, 3.2)
 
 for condition in conditions.keys():
-    series = qr.get_files(
-        subj_id=subject,
-        study=study,
-        modality="MEG",
-        series=conditions[condition])
+    series = qr.get_files(subj_id=subject,
+                          study=study,
+                          modality="MEG",
+                          series=conditions[condition])
 
     raw = mne.io.read_raw_fif(series[0], preload=True)
     raw.info['bads'] = []
-    picks = mne.pick_types(
-        raw.info,
-        meg="grad",
-        eeg=False,
-        stim=False,
-        eog=False,
-        include=[],
-        exclude=[])
+    picks = mne.pick_types(raw.info,
+                           meg="grad",
+                           eeg=False,
+                           stim=False,
+                           eog=False,
+                           include=[],
+                           exclude=[])
 
     raw.info['projs'] = list()  # remove proj
 
-    raw.resample(500, n_jobs=1)
-    raw.filter(fmin, None, fir_design='firwin', n_jobs=n_jobs)
-    raw.filter(None, fmax, fir_design='firwin', n_jobs=n_jobs)
+    raw.resample(500, n_jobs=n_jobs)
+    raw.filter(l_freq, None, fir_design="firwin", n_jobs=n_jobs)
+    raw.filter(None, h_freq, h_trans_bandwidth=8, n_jobs=n_jobs)
+    raw.notch_filter(freqs=50, n_jobs=n_jobs)
 
-    events = mne.find_events(
-        raw,
-        stim_channel='STI101',
-        shortest_event=2,
-        min_duration=0.004,
-        verbose=True)
+    events = mne.find_events(raw,
+                             stim_channel='STI101',
+                             shortest_event=2,
+                             min_duration=0.004,
+                             verbose=True)
     # adjust time delay from tubes
 
-    epochs = mne.Epochs(
-        raw,
-        events,
-        event_id,
-        tmin,
-        tmax,
-        baseline=(None, -3),
-        picks=picks,
-        reject=None,
-        verbose=False,
-        detrend=0,
-        preload=True)
+    epochs = mne.Epochs(raw,
+                        events,
+                        event_id,
+                        tmin,
+                        tmax,
+                        baseline=baseline,
+                        picks=picks,
+                        reject=None,
+                        verbose=False,
+                        detrend=0,
+                        preload=True)
 
-    thresh_func = partial(
-        compute_thresholds,
-        picks=picks,
-        method='random_search',
-        random_state=random_state)
+    thresh_func = partial(compute_thresholds,
+                          picks=picks,
+                          method='random_search',
+                          random_state=random_state)
 
-    ar = LocalAutoRejectCV(
-        n_interpolates, consensus_percs, thresh_func=thresh_func)
+    ar = LocalAutoRejectCV(n_interpolates, consensus, thresh_func=thresh_func)
     ar.fit(epochs)
 
-    epochs.save(erf_raw + "%s_%s_grads_erf-epo.fif" % (subject[:4], condition))
+    epochs.save(erf_raw + "%s_%s_grads_erf_hg-epo.fif" %
+                (subject[:4], condition))
     epochs_clean = ar.transform(epochs)
-    epochs_clean.save(erf_raw + "%s_%s_ar_grads_erf-epo.fif" % (subject[:4],
-                                                                condition))
-    pickle.dump(ar,
-                open(erf_raw + "%s_%s_ar_grads_erf.pkl" %
-                     (subject[:4], condition), "wb"))
+    epochs_clean.save(erf_raw + "%s_%s_ar_grads_erf_hg-epo.fif" %
+                      (subject[:4], condition))
+    pickle.dump(
+        ar,
+        open(erf_raw + "%s_%s_ar_grads_erf_hg.pkl" % (subject[:4], condition),
+             "wb"))
